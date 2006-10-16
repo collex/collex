d60be8629cd4adab16722122b52b227cb5c2b55e
Erik Hatcher <erik.hatcher@gmail.com>
2006-10-16 16:57:33 +0000

    create exhibit wired in, as well as setting the title and license of an exhibit

--- app/controllers/exhibit_controller.rb
+++ app/controllers/exhibit_controller.rb
@@ -3,5 +3,23 @@ class ExhibitController < ApplicationController
   
   def mine
     @exhibit_types = ExhibitType.find(:all)
+    
+    # TODO: look up all exhibits owned by the current user
+  end
+  
+  def new
+    @exhibit = Exhibit.new(:title => "<New>")
+    @exhibit.exhibit_type = ExhibitType.find(params[:type])
+    render :action => 'edit'
+  end
+  
+  def edit
+    @exhibit = Exhibit.find(params[:id])
+  end
+  
+  def update
+    @exhibit = Exhibit.new(params[:exhibit])
+    @exhibit.save
+    redirect_to :action => 'edit', :id => @exhibit
   end
 end
