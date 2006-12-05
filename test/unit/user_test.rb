93b959a729857e751ac9a6e689f5cf02257861fe
Jamie Orchard-Hays <jamieorc@gmail.com>
2006-12-05 20:49:28 +0000

    added assertions for role_names

--- test/unit/user_test.rb
+++ test/unit/user_test.rb
@@ -20,17 +20,20 @@ class UserTest < Test::Unit::TestCase
     assert(user = User.find(@admin_user.id), "Couldn't find admin user.")
     assert(user.roles.size == 1, "User should have one role.")
     assert(user.roles.include?(@admin_role), "User should have admin role.")
+    assert_equal(['admin'], user.role_names)
   end
   
   def test_has_editor_role
     assert(user = User.find(@editor_user.id), "Couldn't find editor user.")
     assert(user.roles.size == 1, "User should have one role.")
     assert(user.roles.include?(@editor_role), "User should have editor role.")
+    assert_equal(['editor'], user.role_names)
   end
   
   def test_has_basic_role
     assert(user = User.find(@basic_user.id), "Couldn't find basic user.")
     assert(user.roles.empty?, "Basic User should have no roles.")
+    assert_equal([], user.role_names)
   end
   
   
