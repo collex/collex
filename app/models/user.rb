9ef53fa0688b9d8a789dbdfc8ccf1c868d544bc5
Jamie Orchard-Hays <jamieorc@gmail.com>
2006-12-05 20:50:32 +0000

    added role_names attribute: read only array of role names for that user

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,8 @@ class User < ActiveRecord::Base
   has_many :interpretations, :dependent => :destroy
   has_many :exhibits
   has_and_belongs_to_many :roles
+  
+  def role_names
+    self.roles.collect { |role| role.name }
+  end
 end
