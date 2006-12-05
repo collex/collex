442eda40b1b7e798d6a6088a4f47984ac88e88d2
Jamie Orchard-Hays <jamieorc@gmail.com>
2006-12-05 19:52:07 +0000

    basic unit tests for Role model

--- test/unit/role_test.rb
+++ test/unit/role_test.rb
@@ -1,10 +1,29 @@
 require File.dirname(__FILE__) + '/../test_helper'
 
 class RoleTest < Test::Unit::TestCase
-  fixtures :roles
+  fixtures :users, :roles, :roles_users
 
+  def setup
+    @admin_user = users "admin"
+    @admin_role = roles "admin"
+    @editor_user = users "editor"
+    @editor_role = roles "editor"
+    @basic_user = users "basic"
+  end
+  
   # Replace this with your real tests.
   def test_truth
     assert true
+  end
+  
+  def test_admin_role_has_admin_user
+    assert(role = Role.find(@admin_role.id), "Failed to find admin role.")
+    assert(role.users.size == 1, "Admin Role should have one user.")
+    assert(role.users.include?(@admin_user), "Admin Role should have admin user.")
+  end
+  def test_editor_role_has_editor_user
+    assert(role = Role.find(@editor_role.id), "Failed to find editor role.")
+    assert(role.users.size == 1, "Editor Role should have one user.")
+    assert(role.users.include?(@editor_user), "Editor Role should have editor user.")
   end
 end
