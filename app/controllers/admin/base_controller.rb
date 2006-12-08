2003cd709c313fcd90107eb9424f513783601a85
Jamie Orchard-Hays <jamieorc@gmail.com>
2006-12-08 03:51:08 +0000

    added the verify method call to the Admin::BaseController

--- app/controllers/admin/base_controller.rb
+++ app/controllers/admin/base_controller.rb
@@ -1,6 +1,10 @@
 class Admin::BaseController < ApplicationController
   layout 'admin'
   before_filter :check_admin_privileges
+  # GETs should be safe (see http://www.w3.org/2001/tag/doc/whenToUseGet.html)
+  verify :method => :post, :only => [ :destroy, :create, :update ],
+         :redirect_to => { :action => :list }
+  
 
   private
   #TODO: move this to a general /admin area controller superclass
