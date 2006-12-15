23b5fa0b78f53583d85cc315415876100503776f
Doug Reside <dougreside@gmail.com>
2006-12-15 20:38:22 +0000

    Layout fixes

--- app/controllers/exchange_controller.rb
+++ app/controllers/exchange_controller.rb
@@ -1,8 +1,8 @@
 class ExchangeController < ApplicationController
 	def index
 		#@exchanges = Exchange.find_by_sql("SELECT * FROM exchanges WHERE task_id = '"+ params['id'] +"'")
-		@exchanges = Exchange.find_by_task_id(params['id'])
+		@exchanges = Exchange.find_all_by_task_id(params['id'])
 		#@errors = Error.find_by_sql("SELECT * FROM errors WHERE task_id = '"+ params['id'] + "'")
-		@errors = Error.find_by_task_id(params['id'])
+		@errors = Error.find_all_by_task_id(params['id'])
 	end
 end
