5c51ed4dcbfe58ff7b70de609cebd272bf2094c1
Jamie Orchard-Hays <jamieorc@gmail.com>
2006-12-20 20:38:18 +0000

    set $KCODE to UTF8 -- necessary for utf-8 support

--- config/environment.rb
+++ config/environment.rb
@@ -6,6 +6,7 @@
 
 # Bootstrap the Rails environment, frameworks, and default configuration
 require File.join(File.dirname(__FILE__), 'boot')
+$KCODE = 'UTF8'
 
 Rails::Initializer.run do |config|
   # Settings in config/environments/* take precedence those specified here
