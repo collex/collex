9443db3f2bc75cd8824d110e19971518b410e444
Jamie Orchard-Hays <jamieorc@gmail.com>
2007-01-24 00:52:56 +0000

    format authors the MLA way (for Annotated Bibliographies)--with tests

--- app/models/resource.rb
+++ app/models/resource.rb
@@ -6,13 +6,36 @@ class Resource < ActiveRecord::Base
   #  resource.title => returns first title property found
   #  resource.titles => returns an array of properties with name "title"
   def method_missing(method_id, *arguments)
-     begin
-       super
-     rescue NoMethodError
-       name = method_id.to_s
-       singular_name = name.singularize
-       props = properties.select {|prop| prop.name == singular_name }
-       return name == singular_name ? props[0] : props
-     end
-   end
+    begin
+      super
+    rescue NoMethodError
+      name = method_id.to_s
+      singular_name = name.singularize
+      props = properties.select {|prop| prop.name == singular_name }
+      return name == singular_name ? props[0] : props
+    end
+  end
+   
+  def to_mla_citation
+    
+  end
+   
+  def mla_authors
+    authors = self.role_AUTs
+    return "---." if authors.size < 1
+    unformatted_names = authors.collect{ |auth| auth.value }
+    names = unformatted_names.collect do |name|
+      if name.include?(", ") then name else n = name.split(" "); n.last + ", " + n[0..-2].join(" ") end
+    end
+    mla_names = case names.size
+    when 1
+      names[0]
+    when 2
+      names.join(" and ")
+    else
+      names[0..-2].join(", ") + ", and " + names.last
+    end
+    mla_names << "." unless mla_names[-1].chr == "."
+    mla_names
+  end
 end
--- test/unit/resource_test.rb
+++ test/unit/resource_test.rb
@@ -1,10 +1,58 @@
 require File.dirname(__FILE__) + '/../test_helper'
 
 class ResourceTest < Test::Unit::TestCase
-  fixtures :resources
+#   fixtures :resources
 
   # Replace this with your real tests.
   def test_truth
     assert true
+  end
+  
+  def test_mla_authors_should_format_single_author
+    @prop_1 = Property.create(:name => "role_AUT", :value => "First Last")
+    @res = Resource.create
+    @res.properties << @prop_1
+    assert_equal(1, @res.properties.size)
+    
+    expected = "Last, First."
+    assert_equal(expected, @res.mla_authors)
+    
+    @res.properties.first.value = "Last, First"
+    @res.save
+    assert_equal(expected, @res.mla_authors)
+  end
+  
+  def test_mla_authors_should_format_two_authors_with_and
+    @prop_1 = Property.create(:name => "role_AUT", :value => "First Last")
+    @prop_2 = Property.create(:name => "role_AUT", :value => "Last-Name, Second")
+    @res = Resource.create
+    @res.properties << @prop_1
+    @res.properties << @prop_2
+    assert_equal(2, @res.properties.size)
+    
+    expected = "Last, First and Last-Name, Second."
+    assert_equal(expected, @res.mla_authors)
+    
+    @res.properties.first.value = "Last, First"
+    @res.save
+    assert_equal(expected, @res.mla_authors)
+  end
+  
+  def test_mla_authors_should_format_many_authors_with_commas_and_and
+    @prop_1 = Property.create(:name => "role_AUT", :value => "First Last")
+    @prop_2 = Property.create(:name => "role_AUT", :value => "Last-Name, Second")
+    @prop_3 = Property.create(:name => "role_AUT", :value => "T.N. Surname")
+    @res = Resource.create
+    @res.properties << @prop_1
+    @res.properties << @prop_2
+    @res.properties << @prop_3
+    assert_equal(3, @res.properties.size)
+    
+    expected = "Last, First, Last-Name, Second, and Surname, T.N."
+    assert_equal(expected, @res.mla_authors)
+    
+    @res.properties.first.value = "Last, First"
+    @res.save
+    assert_equal(expected, @res.mla_authors)
   end
 end
