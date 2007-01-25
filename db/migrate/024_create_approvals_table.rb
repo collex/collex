3587a71878765f5a7d56f960cdf2c05c5e03d4c6
Jamie Orchard-Hays <jamieorc@gmail.com>
2007-01-25 12:50:40 +0000

    fixed white space

--- db/migrate/024_create_approvals_table.rb
+++ db/migrate/024_create_approvals_table.rb
@@ -1,7 +1,7 @@
 class CreateApprovalsTable < ActiveRecord::Migration
   def self.up
   	create_table :approvals do |t|
-	  t.column :task_id, 		:integer,	:null => false
+	    t.column :task_id,	:integer,	:null => false
   	  t.column :uri,			:string,	:null => false
   	  t.column :xml,			:text, 		:null => false
   	  end
