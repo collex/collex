0656193a997b77aaf7457fe89c6434d031ba15f5
Jamie Orchard-Hays <jamieorc@gmail.com>
2007-01-25 12:51:55 +0000

    use ExhibitedSection and convenience method to template

--- app/models/exhibit.rb
+++ app/models/exhibit.rb
@@ -2,6 +2,10 @@ class Exhibit < ActiveRecord::Base
   belongs_to :user
   belongs_to :license
   belongs_to :exhibit_type
-  has_many :sections
+  has_many :sections, :class_name => "ExhibitedSection"
+  
+  def template
+    self.exhibit_type.template
+  end
   
 end
