3486d3632516e1bcf03b94a448782876805a9d86
Jamie Orchard-Hays <jamieorc@gmail.com>
2007-01-30 21:54:10 +0000

    DRYed up the search form for the sidebar by putting in SidebarHelper. Changed the text to "new search" as per bug 1648118.

--- app/helpers/sidebar_helper.rb
+++ app/helpers/sidebar_helper.rb
@@ -1,2 +1,12 @@
 module SidebarHelper
+  def search_form
+    xm = Builder::XmlMarkup.new
+    xm.div(:class => "search") do
+      xm.form(:method => "post", :action => url_for(:controller => "search", :action => "new_expression")) do
+        xm << text_field_tag("field[content]", "new search", :onFocus => "this.value=''")
+        xm << "&nbsp;"
+      end
+    end
+  end
+  
 end
