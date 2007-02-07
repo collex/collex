6ef64a00ef4dcfcb354fe943ba62105bdf14cf61
Jamie Orchard-Hays <jamieorc@gmail.com>
2007-02-07 17:54:05 +0000

    fixed indentation, which was at 3 spaces for first column

--- app/controllers/application.rb
+++ app/controllers/application.rb
@@ -1,40 +1,40 @@
 # The filters added to this controller will be run for all controllers in the application.
 # Likewise will all the methods added be available for all controllers.
 class ApplicationController < ActionController::Base
-   include ExceptionNotifiable
-   
-   filter_parameter_logging "password"
-   
-#   local_addresses.clear  #uncomment to test e-mails locally in development mode
-   
-   layout "common"
-
-   before_filter :set_charset
-   before_filter :session_create
-
-   def boom
-     raise "boom!"
-   end
-   
-   private
-     def session_create
-       session[:constraints] ||= []
-     end
-   
-     def set_charset
-       headers['Content-Type'] = 'text/html; charset=utf-8'
-       headers['Pragma'] = 'no-cache'
-       headers['Cache-Control'] = 'no-cache, no-store, must-revalidate'
-     end
-       
-     def authorize
-       unless session[:user] 
-         flash[:notice] = "please log in" 
-       
-         # save the URL the user requested so we can hop back to it after login
-         session[:jumpto] = request.request_uri if not request.xhr?
-       
-         redirect_to(:controller => "login", :action => "login") 
-       end 
-     end 
+  include ExceptionNotifiable
+  
+  filter_parameter_logging "password"
+  
+#  local_addresses.clear  #uncomment to test e-mails locally in development mode
+  
+  layout "common"
+  
+  before_filter :set_charset
+  before_filter :session_create
+  
+  def boom
+    raise "boom!"
+  end
+  
+  private
+    def session_create
+      session[:constraints] ||= []
+    end
+  
+    def set_charset
+      headers['Content-Type'] = 'text/html; charset=utf-8'
+      headers['Pragma'] = 'no-cache'
+      headers['Cache-Control'] = 'no-cache, no-store, must-revalidate'
+    end
+      
+    def authorize
+      unless session[:user] 
+        flash[:notice] = "please log in" 
+      
+        # save the URL the user requested so we can hop back to it after login
+        session[:jumpto] = request.request_uri if not request.xhr?
+      
+        redirect_to(:controller => "login", :action => "login") 
+      end 
+    end 
 end
