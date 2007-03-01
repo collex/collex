09479a88ada0b16632c60cbc52f85c4f210f66b2
Doug Reside <dougreside@gmail.com>
2007-03-01 04:41:15 +0000

    fixed some database problems with upload

--- app/controllers/overview_controller.rb
+++ app/controllers/overview_controller.rb
@@ -3,8 +3,11 @@ class OverviewController < ApplicationController
 		#@exchanges = Exchange.find_by_sql("SELECT * FROM exchanges WHERE task_id = '"+ params['id'] +"'")
 		@exchanges = Exchange.find_all_by_task_id_and_item(params['id'],"Item added successfully to sql database approval queue")
 		#@errors = Error.find_by_sql("SELECT * FROM errors WHERE task_id = '"+ params['id'] + "'")
-		#@errors = Error.find_all_by_task_id(params['id'])
-		@errors = Error.find(:all).map{ |i| i.uri }.uniq
+		@generalerrors = Error.find_all_by_uri('NA')
+		#@errorsbytask = Error.find_all_by_task_id(params['id')
+
+		@errors = Error.find(:all, :conditions => { :task_id => params['id']}).map{ |i| i.uri }.uniq
+		#
 		#@errors = Error.find(:all, :conditions => {:id => params['id'] })
 		#Student.find(:all, :conditions => { :first_name => "Harvey", :status => 1 })
 	
