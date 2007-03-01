6bb86059cc91b66e01af70027103d461e840ea9d
Doug Reside <dougreside@gmail.com>
2007-03-01 12:18:17 +0000

    More error reporting fixes

--- app/controllers/overview_controller.rb
+++ app/controllers/overview_controller.rb
@@ -3,7 +3,9 @@ class OverviewController < ApplicationController
 		#@exchanges = Exchange.find_by_sql("SELECT * FROM exchanges WHERE task_id = '"+ params['id'] +"'")
 		@exchanges = Exchange.find_all_by_task_id_and_item(params['id'],"Item added successfully to sql database approval queue")
 		#@errors = Error.find_by_sql("SELECT * FROM errors WHERE task_id = '"+ params['id'] + "'")
-		@generalerrors = Error.find_all_by_uri('NA')
+		@generalerrors = Error.find(:all, :conditions => { :task_id=> params['id'], :uri => 'NA'}).map{ |i| i.item }.uniq
+	
+		#Error.find_all_by_uri('NA')
 		#@errorsbytask = Error.find_all_by_task_id(params['id')
 
 		@errors = Error.find(:all, :conditions => { :task_id => params['id']}).map{ |i| i.uri }.uniq
