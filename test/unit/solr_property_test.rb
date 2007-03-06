d7df0c8ae852cdda5d8cb504f0b85d1cdac8fa9d
Jamie Orchard-Hays <jamieorc@gmail.com>
2007-03-06 15:38:46 +0000

    removed fixture, which is not used.

--- test/unit/solr_property_test.rb
+++ test/unit/solr_property_test.rb
@@ -1,7 +1,6 @@
 require File.dirname(__FILE__) + '/../test_helper'
 
 class SolrPropertyTest < Test::Unit::TestCase
-  fixtures :solr_properties
 
   # Replace this with your real tests.
   def test_truth
