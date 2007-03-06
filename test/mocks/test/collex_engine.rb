b236f95dbb68960f1db6529b26528cff3b63d228
Erik Hatcher <erik.hatcher@gmail.com>
2007-03-06 16:35:50 +0000

    Add CollexEngine mock

---
+++ test/mocks/test/collex_engine.rb
@@ -0,0 +1,11 @@
+require File.expand_path(File.dirname(__FILE__) + '/../../../lib/collex_engine')
+class CollexEngine
+  def update(username, uri, tags, annotation)
+  end
+
+  def remove(username, uri)
+  end
+  
+  def commit
+  end
+end
