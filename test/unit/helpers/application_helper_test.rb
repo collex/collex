723fd492aa8559c8442aacb3cec564b1f67214cb
Jamie Orchard-Hays <jamieorc@gmail.com>
2007-03-07 23:44:18 +0000

    updated to recognize new thumbnail_<uri> id

--- test/unit/helpers/application_helper_test.rb
+++ test/unit/helpers/application_helper_test.rb
@@ -5,6 +5,10 @@ class ApplicationHelperTest < HelperTestCase
   include ApplicationHelper
 
   #fixtures :users, :articles
+  URI = 'http://test/uri'
+  def setup
+    @item = {'uri' => URI}
+  end
 
   # mock of ApplicationHelper#site()
   def site(code)
@@ -22,32 +26,32 @@ class ApplicationHelperTest < HelperTestCase
   end
   
   def test_thumbnail_image_tag_should_get_generic_image_when_no_others
-    item = {'archive' => 'generic', 'title' => 'The Generic'}
-    expected = %(<img src="#{DEFAULT_THUMBNAIL_IMAGE_PATH}" alt="The Generic" align="left"/>)
+    item = @item.merge({'archive' => 'generic', 'title' => 'The Generic'})
+    expected = %(<img src="#{DEFAULT_THUMBNAIL_IMAGE_PATH}" alt="The Generic" align="left" id="thumbnail_#{URI}"/>)
     result = thumbnail_image_tag(item)
     puts result
     assert_dom_equal(expected, result)    
   end
   
   def test_thumbnail_image_tag_should_get_site_image_when_no_specific_thumbnail
-    item = {'archive' => 'site', 'title' => 'Specific Site Title'}
-    expected = %(<img src="http://some.site.url.com/image.gif" alt="Specific Site Title" align="left"/>)
+    item = @item.merge({'archive' => 'site', 'title' => 'Specific Site Title'})
+    expected = %(<img src="http://some.site.url.com/image.gif" alt="Specific Site Title" align="left" id="thumbnail_#{URI}"/>)
     result = thumbnail_image_tag(item)
     puts result
     assert_dom_equal(expected, result)
   end
   
   def test_thumbnail_image_tag_should_get_thumbnail_of_specific_item_when_present
-    item = {'archive' => 'site', 'title' => 'Specific Site Title', 'thumbnail' => 'http://some.specific.url.com/image.gif'}
-    expected = %(<img src="http://some.specific.url.com/image.gif" alt="Specific Site Title" align="left"/>)
+    item = @item.merge({'archive' => 'site', 'title' => 'Specific Site Title', 'thumbnail' => 'http://some.specific.url.com/image.gif'})
+    expected = %(<img src="http://some.specific.url.com/image.gif" alt="Specific Site Title" align="left" id="thumbnail_#{URI}"/>)
     result = thumbnail_image_tag(item)
     puts result
     assert_dom_equal(expected, result)
   end
 
   def test_thumbnail_image_tag_without_extension
-    item = {'archive' => 'site', 'title' => 'Specific Site Title', 'thumbnail' => 'http://www.purl.org/swinburnearchive/img/tsa9thmb00/'}
-    expected = %(<img src="http://www.purl.org/swinburnearchive/img/tsa9thmb00/" alt="Specific Site Title" align="left"/>)
+    item = @item.merge({'archive' => 'site', 'title' => 'Specific Site Title', 'thumbnail' => 'http://www.purl.org/swinburnearchive/img/tsa9thmb00/'})
+    expected = %(<img src="http://www.purl.org/swinburnearchive/img/tsa9thmb00/" alt="Specific Site Title" align="left" id="thumbnail_#{URI}"/>)
     result = thumbnail_image_tag(item)
     puts result
     assert_dom_equal(expected, result)
