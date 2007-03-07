538c74a31f96e1334a56ed8da2eccdd72bbd2f73
Jamie Orchard-Hays <jamieorc@gmail.com>
2007-03-07 23:38:39 +0000

    made abstract and added create_or_update

--- app/models/solr_base_model.rb
+++ app/models/solr_base_model.rb
@@ -1,6 +1,10 @@
 class SolrBaseModel < ActiveRecord::Base
   # From: http://rails.techno-weenie.net/forums/2/topics/724
-
+  self.abstract_class = true
+  def create_or_update
+    errors.empty?
+  end
+  
   def self.columns() @columns ||= []; end
   def self.column(name, sql_type = nil, default = nil, null = true)
     columns << ActiveRecord::ConnectionAdapters::Column.new(name.to_s, default, sql_type.to_s, null)
