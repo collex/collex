7cdaa3f48049abb40022d6521e24ad666c086928
Jamie Orchard-Hays <jamieorc@gmail.com>
2007-03-13 15:26:10 +0000

    access the resource's properties as if they were properties of the exhibited resource

--- app/models/exhibited_resource.rb
+++ app/models/exhibited_resource.rb
@@ -7,4 +7,14 @@ class ExhibitedResource < ActiveRecord::Base
     @resource ||= SolrResource.find_by_uri(self.uri)
   end
   
+  # Make the resource's properties transparent
+  def method_missing(method_id, *arguments)
+    begin
+      super
+    rescue NoMethodError
+      name = method_id.to_s
+      self.resource.blank? ? nil : self.resource.__send__(name)
+    end
+  end
+  
 end
