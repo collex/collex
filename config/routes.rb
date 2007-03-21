68d2bdf7d4014e423442bcc2fed5c6e793b3cdac
Jamie Orchard-Hays <jamieorc@gmail.com>
2007-03-21 22:56:04 +0000

    Routes for the Ajax calls in Exhibit Builder

--- config/routes.rb
+++ config/routes.rb
@@ -20,9 +20,10 @@ ActionController::Routing::Routes.draw do |map|
                     :value => /[^\/]+/,
                     :user => nil
 
-  map.resources :exhibits, :singular => "exhibit", :member => { :add_resource => :post } do |exhibit|
-    exhibit.resources :exhibited_sections do |section|
-      section.resources :exhibited_resources
+  map.resources :exhibits, :singular => "exhibit", 
+                :member => { :add_resource => :post, :update_title => :post, :update_annotation => :post } do |exhibit|
+    exhibit.resources :exhibited_sections, :member => { :update_title => :post, :update_annotation => :post } do |section|
+      section.resources :exhibited_resources, :member => { :update_annotation => :post }
     end
   end
   
