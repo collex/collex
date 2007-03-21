cb9a188d43fe26fc14cc1f3f72386668596dcca2
Jamie Orchard-Hays <jamieorc@gmail.com>
2007-03-21 19:07:02 +0000

    added specialized in_place_editor methods for the exhibits

--- app/helpers/exhibits_helper.rb
+++ app/helpers/exhibits_helper.rb
@@ -1,2 +1,42 @@
 module ExhibitsHelper
+  def exhibit_in_place_editor_field(object, method, tag_options = {}, in_place_editor_options = {}, external_control_options = {})
+        
+    tag = ::ActionView::Helpers::InstanceTag.new(object, method, self)
+    tag_options = {:tag => "span", :id => "#{object}_#{method}_#{tag.object.id}_in_place_editor", :class => "in_place_editor_field"}.merge!(tag_options)
+    in_place_editor_options[:url] ||= 
+    eval("update_#{method}_#{object}_path(#{tag.object.id})") rescue url_for({ :action => "set_#{object}_#{method}", :id => tag.object.id })
+    in_place_editor_options[:external_control] ||= "#{object}_#{method}_#{tag.object.id}_external_control"
+    in_place_editor_options[:save_text] ||= "save #{object.to_s.humanize.downcase} #{method.to_s.humanize.downcase}"
+    in_place_editor_options[:saving_text] ||= "saving #{object.to_s.humanize.downcase} #{method.to_s.humanize.downcase}..."
+    in_place_editor_options[:size] ||= 35
+    
+    external_control_options = { :text => "edit #{object.to_s.humanize.downcase} #{method.to_s.humanize.downcase}", :class => "exhibit_external_control", :href => "javascript: void(0);", :id => in_place_editor_options[:external_control] }.merge(external_control_options)
+    
+    tag.to_content_tag(tag_options.delete(:tag), tag_options) + "&nbsp;" +
+    a(external_control_options.delete(:text), external_control_options) +
+    in_place_editor(tag_options[:id], in_place_editor_options)
+  end
+  
+  # Since Rails currently (1.2.1) does not generate proper URLs for nested resources without
+  # the parent objects specified, this is a convenience
+  def exhibited_section_in_place_editor_field(object, method, tag_options = {}, in_place_editor_options = {}, external_control_options = {})
+    tag = ::ActionView::Helpers::InstanceTag.new(object, method, self)
+    in_place_editor_options[:url] ||=  eval("update_#{method}_#{object}_path(#{tag.object.exhibit.id}, #{tag.object.id})") 
+    exhibit_in_place_editor_field(object, method, tag_options, in_place_editor_options, external_control_options)
+  end
+  def exhibited_section_in_place_editor_area(object, method, tag_options = {}, in_place_editor_options = {}, external_control_options = {})
+    in_place_editor_options[:rows] = 12
+    in_place_editor_options[:cols] = 35
+    exhibited_section_in_place_editor_field(object, method, tag_options, in_place_editor_options, external_control_options)
+  end
+  def exhibited_resource_in_place_editor_field(object, method, tag_options = {}, in_place_editor_options = {}, external_control_options = {})
+    tag = ::ActionView::Helpers::InstanceTag.new(object, method, self)
+    in_place_editor_options[:url] ||=  eval("update_#{method}_#{object}_path(#{tag.object.exhibited_section.exhibit.id}, #{tag.object.exhibited_section.id}, #{tag.object.id})") 
+    exhibit_in_place_editor_field(object, method, tag_options, in_place_editor_options, external_control_options)
+  end
+  def exhibited_resource_in_place_editor_area(object, method, tag_options = {}, in_place_editor_options = {}, external_control_options = {})
+    in_place_editor_options[:rows] = 12
+    in_place_editor_options[:cols] = 35
+    exhibited_resource_in_place_editor_field(object, method, tag_options, in_place_editor_options, external_control_options)
+  end
 end
