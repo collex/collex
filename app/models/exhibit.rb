2f5746fd02aeb9c8d33f57830ce41e19e64d170f
Jamie Orchard-Hays <jamieorc@gmail.com>
2007-03-21 20:58:21 +0000

    validate required fields

--- app/models/exhibit.rb
+++ app/models/exhibit.rb
@@ -4,6 +4,8 @@ class Exhibit < ActiveRecord::Base
   belongs_to :exhibit_type
   has_many :exhibited_sections, :order => "position"
   
+  validates_presence_of :title, :license_id, :exhibit_type_id, :user_id
+  
   def template
     self.exhibit_type.template
   end
