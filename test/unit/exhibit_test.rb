aff77de06576b615a108f9d61a2393b3cf9c46a8
Jamie Orchard-Hays <jamieorc@gmail.com>
2007-03-21 22:04:54 +0000

    tests for owner?(user)

--- test/unit/exhibit_test.rb
+++ test/unit/exhibit_test.rb
@@ -2,9 +2,31 @@ require File.dirname(__FILE__) + '/../test_helper'
 
 class ExhibitTest < Test::Unit::TestCase
   fixtures :exhibits
+  
+  def setup
+    @exhibit = Exhibit.new
+    @exhibit.license_id = 2
+    @exhibit.exhibit_type_id = 2
+    @exhibit.title = "Title"
+    @user = User.new
+    @user.save!
+    @exhibit.user = @user
+    @exhibit.save!
+  end
 
-  # Replace this with your real tests.
-  def test_truth
-    assert true
+  def test_owner_is_true_for_user_object
+    assert(@exhibit.owner?(@user), "User did not equal owner for exhibit.")
+  end
+  
+  def test_owner_is_true_for_user_id
+    assert(@exhibit.owner?(@user.id), "user_id did not equal owner for exhibit.")
+  end
+  
+  def test_bad_owner_is_false_for_user_object
+    assert(!@exhibit.owner?(User.new), "User should not be owner")
+  end
+  
+  def test_bad_owner_is_false_for_user_id
+    assert(!@exhibit.owner?(-1), "User should not be owner")
   end
 end
