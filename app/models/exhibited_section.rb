6ce955f669571e0f89dba2f43f1d0ce0e1a8fe63
Jamie Orchard-Hays <jamieorc@gmail.com>
2007-04-02 21:58:15 +0000

    just show one section per page

--- app/models/exhibited_section.rb
+++ app/models/exhibited_section.rb
@@ -1,9 +1,11 @@
 class ExhibitedSection < ActiveRecord::Base
   has_many :exhibited_resources, :order => "position", :dependent => :destroy
-#   has_many :resources, :class_name => "SolrResource", :through => :exhibited_resources
   belongs_to :exhibit_section_type
   belongs_to :exhibit
   acts_as_list
+  
+  # Just show one section per page by default
+  set_page_size 1
   
   def template
     self.exhibit_section_type.template
