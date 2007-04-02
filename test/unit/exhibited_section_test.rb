93288b1abf40c890a51d265c91c32ea3ad6f865f
Jamie Orchard-Hays <jamieorc@gmail.com>
2007-04-02 21:58:37 +0000

    a couple of tests for the PaginatingFind plugin

--- test/unit/exhibited_section_test.rb
+++ test/unit/exhibited_section_test.rb
@@ -1,10 +1,15 @@
 require File.dirname(__FILE__) + '/../test_helper'
 
 class ExhibitedSectionTest < Test::Unit::TestCase
-#   fixtures :exhibited_sections
+  fixtures :exhibited_sections
 
-  # Replace this with your real tests.
-  def test_truth
-    assert true
+  # some testing for the paginating_find plugin enhancements
+  def test_find_all_returns_all_exhibited_sections
+    assert(ExhibitedSection.find(:all).size > 1)
   end
+  # this tests an enhancement to PaginatingFind: if Model has class reader page_size, this is automatically used
+  def test_find_all_with_page_returns_page_size_number_of_items
+    assert_equal(ExhibitedSection.page_size, ExhibitedSection.find(:all, :page).page_size)
+  end
+
 end
