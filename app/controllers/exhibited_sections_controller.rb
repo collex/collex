845387141ba0d016e3cb29289ea4b993775cc276
Jamie Orchard-Hays <jamieorc@gmail.com>
2007-04-03 19:20:44 +0000

    added create action

--- app/controllers/exhibited_sections_controller.rb
+++ app/controllers/exhibited_sections_controller.rb
@@ -3,6 +3,25 @@ class ExhibitedSectionsController < ApplicationController
   in_place_edit_for_resource :exhibited_section, :title
   in_place_edit_for_resource :exhibited_section, :annotation
   
+  def create
+    @exhibit = Exhibit.find(params[:exhibit_id])
+    respond_to do |format|
+      if @exhibit.exhibited_sections << ExhibitedSection.new(:exhibit_section_type_id => params[:exhibit_section_type_id])
+        @exhibit.exhibited_sections.last.move_to_top
+        flash[:notice] = 'A new Exhibited Section was successfully added.'
+        format.html { redirect_to edit_exhibit_url(:id => @exhibit) }
+        format.xml  { head :ok }
+      else
+        format.html do
+          flash[:error] = "There was a problem creating a new Exibited Section."
+          redirect_to edit_exhibit_url(@exhibit)
+        end
+        format.xml  { render :xml => @exhibited_section.errors.to_xml }
+      end
+    end
+    
+  end
+  
   def update
     @exhibited_section = ExhibitedSection.find(params[:id])
     @exhibit = Exhibit.find(params[:exhibit_id])
