486558209d0120d4b9e025516ae81840f7345570
Jamie Orchard-Hays <jamieorc@gmail.com>
2007-04-03 19:22:37 +0000

    removed the external controls. We just want to click the text and edit

--- app/helpers/exhibits_helper.rb
+++ app/helpers/exhibits_helper.rb
@@ -6,17 +6,12 @@ module ExhibitsHelper
     
     in_place_editor_options[:url] ||= 
     eval("update_#{method}_#{object}_path(#{tag.object.id})") rescue url_for({ :action => "set_#{object}_#{method}", :id => tag.object.id })
-    in_place_editor_options[:external_control] ||= "#{object}_#{method}_#{tag.object.id}_external_control"
-    in_place_editor_options[:save_text] ||= "save #{object.to_s.humanize.downcase} #{method.to_s.humanize.downcase}"
     in_place_editor_options[:saving_text] ||= "saving #{object.to_s.humanize.downcase} #{method.to_s.humanize.downcase}..."
     in_place_editor_options[:size] ||= 35
-    
-    external_control_options = { :text => "edit #{object.to_s.humanize.downcase} #{method.to_s.humanize.downcase}", :class => "exhibit_external_control", :href => "javascript: void(0);", :id => in_place_editor_options[:external_control] }.merge(external_control_options)
     
 #     tag.to_content_tag(tag_options.delete(:tag), tag_options) + "&nbsp;" +    
     value = tag.value(tag.object).blank? ? "(No #{method} given)" : tag.value(tag.object)
     tag.content_tag(tag_options.delete(:tag), value, tag_options) + "&nbsp;" +
-    a(external_control_options.delete(:text), external_control_options) +
     in_place_editor(tag_options[:id], in_place_editor_options)
   end
   def exhibit_in_place_editor_area(object, method, tag_options = {}, in_place_editor_options = {}, external_control_options = {})
