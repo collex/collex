17cac640e53f6f7084e9dde8e9fc10b4daf67dc1
Erik Hatcher <erik.hatcher@gmail.com>
2007-05-01 16:12:20 +0000

    hardcode in jarrys IP address to be considered local also, so adminapp can send site updates to it

--- app/controllers/admin_message_controller.rb
+++ app/controllers/admin_message_controller.rb
@@ -16,6 +16,12 @@ class AdminMessageController < ApplicationController
   end
 
   private
+  def local_request?
+    request_ips = [request.remote_addr, request.remote_ip]
+    # locahost or jarry hardcoded right now
+    (request_ips == ["127.0.0.1"] * 2) || (request_ips == ['128.143.21.77'] * 2)
+  end
+  
   def allow_only_local
     if !(local_request?)
       render :text => "Only local requests supported", :status => 401
