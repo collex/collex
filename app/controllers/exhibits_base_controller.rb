2678e395de31280356703cefd061a152d18167cd
Jamie Orchard-Hays <jamieorc@gmail.com>
2007-05-02 15:29:42 +0000

    added some useful logging

--- app/controllers/exhibits_base_controller.rb
+++ app/controllers/exhibits_base_controller.rb
@@ -4,10 +4,12 @@ class ExhibitsBaseController < ApplicationController
     def authorize_owner
       @exhibit = Exhibit.find(params[:id])
       unless @exhibit.owner?(user)
+        logger.info("#{user.username} is not owner of #{@exhibit.title} (id=#{@exhibit.id})")
         flash[:warning] = "You do not have permission to edit that Exhibit!"
         redirect_to(exhibits_path) and return false
       end
     rescue ActiveRecord::RecordNotFound
+      logger.info("Exhibit with id #{params[:id]} not found.")
       flash[:warning] = "That Exhibit could not be found."
       redirect_to exhibits_path
     end
