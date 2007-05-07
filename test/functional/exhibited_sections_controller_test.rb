ddccb84f45752d7bf10c1b09bf26d42a664f49cb
Jamie Orchard-Hays <jamieorc@gmail.com>
2007-05-07 18:28:15 +0000

    Exhibited Sections tests for move_higher, move_lower, move_to_top, and move_to_bottom, as well as authentication.

--- test/functional/exhibited_sections_controller_test.rb
+++ test/functional/exhibited_sections_controller_test.rb
@@ -5,14 +5,71 @@ require 'exhibited_sections_controller'
 class ExhibitedSectionsController; def rescue_action(e) raise e end; end
 
 class ExhibitedSectionsControllerTest < Test::Unit::TestCase
+  fixtures :exhibits, :exhibited_resources, :exhibited_sections, :users
+  fixtures :licenses, :exhibit_section_types, :exhibit_types
   def setup
     @controller = ExhibitedSectionsController.new
     @request    = ActionController::TestRequest.new
     @response   = ActionController::TestResponse.new
+    @owner = users(:exhibit_owner)
+    @viewer = users(:exhibit_viewer)
+    @request.session[:user] = {:username => @owner.username}
+
+    @exhibit = exhibits(:dang)
+    @es1 = exhibited_sections(:dang_1)
+    @es2 = exhibited_sections(:dang_2)
+    @es3 = exhibited_sections(:dang_3)
   end
 
-  # Replace this with your real tests.
-  def test_truth
-    assert true
+  def test_redirects_to_login_if_not_logged_in
+    @request.session[:user] = nil
+    post(:move_higher, :id => @es2.id, :exhibit_id => @exhibit.id)
+    assert_response(:redirect)
+    assert_redirected_to(:action => "login", :controller => "login")
+  end
+
+  def test_redirects_to_exhibits_index_if_not_owner
+    @request.session[:user] = {:username => @viewer.username}
+    post(:move_higher, :id => @es2.id, :exhibit_id => @exhibit.id)
+    assert_response(:redirect)
+    assert_redirected_to(exhibits_path)
+  end
+  def test_moves_section_higher_and_returns_to_proper_page
+    assert(@es2.position > @es1.position)
+    post(:move_higher, :id => @es2.id, :exhibit_id => @exhibit.id, :page => 1)
+    @es2.reload
+    @es1.reload
+    assert(@es2.position < @es1.position)
+    assert_response(:redirect)
+    assert_redirected_to(edit_exhibit_path(:id => @exhibit, :page => @request.params[:page], :anchor => "exhibited_section_#{@es2.id}"))
+  end
+  def test_moves_section_lower_and_returns_to_proper_page
+    assert(@es2.position < @es3.position)
+    post(:move_lower, :id => @es2.id, :exhibit_id => @exhibit.id, :page => 1)
+    @es2.reload
+    @es3.reload
+    assert(@es2.position > @es3.position)
+    assert_response(:redirect)
+    assert_redirected_to(edit_exhibit_path(:id => @exhibit, :page => @request.params[:page], :anchor => "exhibited_section_#{@es2.id}"))
+  end
+  def test_moves_section_to_top_and_returns_to_proper_page
+    assert(@es1.position < @es2.position && @es2.position < @es3.position)
+    post(:move_to_top, :id => @es3.id, :exhibit_id => @exhibit.id, :page => 1)
+    @es1.reload
+    @es2.reload
+    @es3.reload
+    assert(@es2.position > @es1.position && @es1.position > @es3.position)
+    assert_response(:redirect)
+    assert_redirected_to(edit_exhibit_path(:id => @exhibit, :page => @request.params[:page], :anchor => "exhibited_section_#{@es3.id}"))
+  end
+  def test_moves_section_to_bottom_and_returns_to_proper_page
+    assert(@es1.position < @es2.position && @es2.position < @es3.position)
+    post(:move_to_bottom, :id => @es1.id, :exhibit_id => @exhibit.id, :page => 1)
+    @es1.reload
+    @es2.reload
+    @es3.reload
+    assert(@es1.position > @es3.position && @es3.position > @es2.position)
+    assert_response(:redirect)
+    assert_redirected_to(edit_exhibit_path(:id => @exhibit, :page => @request.params[:page], :anchor => "exhibited_section_#{@es1.id}"))
   end
 end
