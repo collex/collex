710e682ff4e3f847bc8180b66fc3b1acc1352efa
Jamie Orchard-Hays <jamieorc@gmail.com>
2007-05-15 20:59:47 +0000

    Preliminary work on permissions for Exhibits including tests and additional fields for fixture.

--- app/models/exhibit.rb
+++ app/models/exhibit.rb
@@ -18,5 +18,30 @@ class Exhibit < ActiveRecord::Base
   def owner?(user)
     user.is_a?(Integer) ? self.user_id == user : self.user_id == user.id rescue false
   end
+    
+# Permissions
+  def shared(value)
+    shared = value unless published?
+  end
+
+  def publishable?
+    shared?
+  end
+
+  def viewable_by?(viewer)
+    shared? or viewer == user
+  end
+
+  def updatable_by?(editor)
+    editor == user
+  end
+
+  def deletable_by?(deleter)
+    deleter == user
+  end
+
+  def creatable_by?(creator)
+    !creator.guest?
+  end  
   
 end
--- test/unit/exhibits_test.rb
+++ test/unit/exhibits_test.rb
@@ -29,4 +29,35 @@ class ExhibitsTest < Test::Unit::TestCase
   def test_owner_is_false_for_non_owner_id
     assert( !@exhibit.owner?(@owner.id + 1), "owner? should have responded false.")
   end
+  
+  # test permissions
+  def test_owner_can_view_exhibit
+    assert @exhibit.viewable_by?(@owner)
+  end
+  
+  def test_non_owner_can_not_view_unshared_exhibit
+    assert(!@exhibit.viewable_by?(User.new), "Non-owner should not be able to view unshared exhibit.")
+  end
+  
+  def test_owner_can_update_exhibit
+    assert(@exhibit.updatable_by?(@owner), "Owner should be able to update exhibit.")
+  end
+  
+  def test_non_owner_can_not_update_exhibit
+    assert(!@exhibit.updatable_by?(User.new), "Non-owner should not be able to update exhibit.")
+  end
+  
+  def test_owner_can_delete_exhibit
+    assert(@exhibit.deletable_by?(@owner), "Owner should be able to delete exhibit")
+  end
+  
+  def test_non_owner_can_not_delete_exhibit
+    assert(!@exhibit.deletable_by?(User.new), "Non-owner should not be able to delete another's exhibit.")
+  end
+  
+  def test_anyone_can_view_shared_exhibit
+    @exhibit.shared = true
+    assert(@exhibit.viewable_by?(User.new), "Anyone should be able to view a shared exhibit.")
+  end
+
 end
