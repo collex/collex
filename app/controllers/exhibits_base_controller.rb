45b8bd330474f429b55ac5df19cd40facffacef6
Jamie Orchard-Hays <jamieorc@gmail.com>
2007-05-29 16:10:40 +0000

    converted to use updatable_by? and user_or_guest

--- app/controllers/exhibits_base_controller.rb
+++ app/controllers/exhibits_base_controller.rb
@@ -4,8 +4,8 @@ class ExhibitsBaseController < ApplicationController
     def authorize_owner
       id = params[:exhibit_id] || params[:id]
       @exhibit = Exhibit.find(id)
-      unless @exhibit.owner?(user)
-        logger.info("#{user.username} is not owner of #{@exhibit.title} (id=#{@exhibit.id})")
+      unless @exhibit.updatable_by?(user_or_guest)
+        logger.info("#{user_or_guest.username} is not owner of #{@exhibit.title} (id=#{@exhibit.id})")
         flash[:warning] = "You do not have permission to edit that Exhibit!"
         redirect_to(exhibits_path) and return false
       end
