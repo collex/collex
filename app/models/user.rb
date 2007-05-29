456454afef82ca166623b30873e50596c23ac60b
Jamie Orchard-Hays <jamieorc@gmail.com>
2007-05-29 16:51:04 +0000

    trim off "_role?" when checking the users role

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,7 +11,7 @@ class User < ActiveRecord::Base
   # Added boolean convenience attribute for role names
   def method_missing(method, *args, &block)
     if method.to_s =~ /_role\?$/ 
-      role_names.include?(method.to_s.chop)
+      role_names.include?(method.to_s[0..-7])
     else
       super
     end
