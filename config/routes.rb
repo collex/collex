f64413e40c9a368669b0d0308cbae8959d60ccc7
Jamie Orchard-Hays <jamieorc@gmail.com>
2007-05-29 18:53:52 +0000

    added share and publish to exhibit route methods

--- config/routes.rb
+++ config/routes.rb
@@ -23,6 +23,8 @@ ActionController::Routing::Routes.draw do |map|
   map.resources :exhibits, :singular => "exhibit", 
                 :member => { :add_resource      => :post, 
                              :update_title      => :post, 
+                             :share             => :post, 
+                             :publish           => :post, 
                              :update_annotation => :post } do |exhibit|
     exhibit.resources :exhibited_sections, 
                       :member => { :update_title      => :post, 
