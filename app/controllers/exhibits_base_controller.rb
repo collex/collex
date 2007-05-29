5fd5454e703371c55a384479952e935a95af21a7
Jamie Orchard-Hays <jamieorc@gmail.com>
2007-05-29 21:38:14 +0000

    authorize_viewer makes checks that the exhibit is shared for the general public and restricts access otherwise

--- app/controllers/exhibits_base_controller.rb
+++ app/controllers/exhibits_base_controller.rb
@@ -1,6 +1,7 @@
 class ExhibitsBaseController < ApplicationController
   prepend_before_filter :authorize, :except => [:index, :show]
   before_filter :authorize_owner, :except => [:create, :new, :index, :show]
+  before_filter :authorize_viewer, :only => [:show]
   
   private
     def authorize_owner
@@ -9,6 +10,20 @@ class ExhibitsBaseController < ApplicationController
       unless @exhibit.updatable_by?(user_or_guest)
         logger.info("#{user_or_guest.username} is not owner of #{@exhibit.title} (id=#{@exhibit.id})")
         flash[:warning] = "You do not have permission to edit that Exhibit!"
+        redirect_to(exhibits_path) and return false
+      end
+    rescue ActiveRecord::RecordNotFound
+      logger.info("Exhibit with id #{id} not found.")
+      flash[:warning] = "That Exhibit could not be found."
+      redirect_to exhibits_path
+    end
+    
+    def authorize_viewer
+      id = params[:exhibit_id] || params[:id]
+      @exhibit = Exhibit.find(id)
+      unless @exhibit.viewable_by?(user_or_guest)
+        logger.info("#{user_or_guest.username} is not owner of #{@exhibit.title} (id=#{@exhibit.id})")
+        flash[:warning] = "That exhibit has not been shared."
         redirect_to(exhibits_path) and return false
       end
     rescue ActiveRecord::RecordNotFound
