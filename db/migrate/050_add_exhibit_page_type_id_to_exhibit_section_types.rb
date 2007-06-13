554f0d9509501be304e7ede2ac31dece767eec67
Jamie Orchard-Hays <jamieorc@gmail.com>
2007-06-13 03:45:53 +0000

    rescue loading of data for test db in CC.rb

--- db/migrate/050_add_exhibit_page_type_id_to_exhibit_section_types.rb
+++ db/migrate/050_add_exhibit_page_type_id_to_exhibit_section_types.rb
@@ -4,14 +4,17 @@ class AddExhibitPageTypeIdToExhibitSectionTypes < ActiveRecord::Migration
   
   def self.up
     add_column :exhibit_section_types, :exhibit_page_type_id, :integer
-    @est = ExhibitSectionType.find(1)
-    @est.update_attribute(:exhibit_page_type_id, 1)
-    @est = ExhibitSectionType.find(2)
-    @est.update_attribute(:exhibit_page_type_id, 2)
-    @est = ExhibitSectionType.find(3)
-    @est.update_attribute(:exhibit_page_type_id, 2)
-    @est = ExhibitSectionType.find(4)
-    @est.update_attribute(:exhibit_page_type_id, 2)
+    begin
+      @est = ExhibitSectionType.find(1)
+      @est.update_attribute(:exhibit_page_type_id, 1)
+      @est = ExhibitSectionType.find(2)
+      @est.update_attribute(:exhibit_page_type_id, 2)
+      @est = ExhibitSectionType.find(3)
+      @est.update_attribute(:exhibit_page_type_id, 2)
+      @est = ExhibitSectionType.find(4)
+      @est.update_attribute(:exhibit_page_type_id, 2)
+    rescue
+    end
   end
 
   def self.down
