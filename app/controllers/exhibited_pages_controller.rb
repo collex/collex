bc24c79f3b90f26e52b7efb78a62ad90d583ef14
Jamie Orchard-Hays <jamieorc@gmail.com>
2007-06-18 23:17:11 +0000

    added create(), @licenses for edit(), before_filter, ExhibitsHelper, and the move_higer, lower, etc code for reorganizing order of ExhibitedPages in an Exhibit.

--- app/controllers/exhibited_pages_controller.rb
+++ app/controllers/exhibited_pages_controller.rb
@@ -1,5 +1,35 @@
 class ExhibitedPagesController < ExhibitsBaseController
   layout "nines"
+  helper ExhibitsHelper
+  before_filter :authorize_owner, :except => [:show]
+  
+  in_place_edit_for_resource :exhibited_page, :title
+  in_place_edit_for_resource :exhibited_page, :annotation
+
+  def move_higher
+    move_item(:move_higher, "Moved Page Up.")
+  end  
+  def move_lower
+    move_item(:move_lower, "Moved Page Down.")
+  end  
+  def move_to_top
+    move_item(:move_to_top, "Moved Page to Top.")
+  end  
+  def move_to_bottom
+    move_item(:move_to_bottom, "Moved Page to Bottom.")
+  end
+  def move_item(command, notice)
+    @exhibited_page = @exhibit.exhibited_pages.find(params[:id])
+    @exhibited_page.__send__(command)
+    logger.info("ExhibitedPage: #{command.to_s}: #{params[:id]}")
+    flash[:notice] = notice
+    redirect_to edit_page_path(:id => @exhibited_page, :exhibit_id => @exhibit)
+  rescue Exception => e
+    logger.info("Error: #{command} with id=#{params[:id]} failed with #{e}")
+    flash[:error] = "There was an error moving your page."
+    redirect_to edit_page_path(:id => @exhibited_page, :exhibit_id => @exhibit)
+  end
+  private :move_item
   
   # Note @exhibit is populated in ExhibitsBaseController except for index
   def index
@@ -13,8 +43,23 @@ class ExhibitedPagesController < ExhibitsBaseController
 
   def edit
     @exhibited_page = @exhibit.exhibited_pages.find(params[:id])
+    @licenses = License.find(:all)    
   end
 
   def new
+  end
+  
+  # Note, just have one page type per exhibit right now, so grab the first
+  def create
+#     @exhibit = Exhibit.find(params[:exhibit_id])
+    flash[:notice] = 'Page was successfully created.'
+    page_type = @exhibit.valid_page_types.first
+    if @page = @exhibit.pages.create({:exhibit_page_type_id => page_type.id})
+      flash[:notice] = 'Page was successfully created.'
+      redirect_to edit_page_path(:id => @page, :exhibit_id => @exhibit)
+    else
+      flash[:error] = 'There was an error creating the new page.'
+      redirect_to edit_page_path(:id => params[:page_id], :exhibit_id => @exhibit)
+    end
   end
 end
--- app/controllers/exhibited_sections_controller.rb
+++ app/controllers/exhibited_sections_controller.rb
@@ -16,31 +16,31 @@ class ExhibitedSectionsController < ExhibitsBaseController
     move_item(:move_to_bottom, "Moved Exhibited Section to Bottom.")
   end
   def move_item(command, notice)
-    @exhibited_section = @exhibit.exhibited_pages.find(params[:exhibited_page_id]).exhibited_sections.find(params[:id])
+    @exhibited_section = @exhibit.exhibited_pages.find(params[:page_id]).exhibited_sections.find(params[:id])
     @exhibited_section.__send__(command)
     logger.info("ExhibitedSection: #{command.to_s}: #{params[:id]}")
     flash[:notice] = notice
-    page = params[:page] || 1
-    redirect_to edit_exhibit_path(:id => params[:exhibit_id], :anchor => dom_id(@exhibited_section), :page => page)
+    redirect_to edit_page_path(:id => params[:page_id], :exhibit_id => @exhibit, :anchor => dom_id(@exhibited_section))
   rescue Exception => e
     logger.info("Error: #{command} with id=#{params[:id]} failed with #{e}")
     flash[:error] = "There was an error moving your section."
-    redirect_to edit_exhibit_path(:id => params[:exhibit_id], :page => page)
+    redirect_to edit_page_path(:id => params[:page_id], :exhibit_id => @exhibit)
   end
   private :move_item  
 
   def create
     @exhibit = Exhibit.find(params[:exhibit_id])
+    @exhibited_page = @exhibit.exhibited_pages.find(params[:page_id])
     respond_to do |format|
-      if @exhibit.exhibited_sections << ExhibitedSection.new(:exhibit_section_type_id => params[:exhibit_section_type_id])
-        @exhibit.exhibited_sections.last.move_to_top
+      if @exhibited_page.exhibited_sections << ExhibitedSection.new(:exhibit_section_type_id => params[:exhibit_section_type_id])
+        @exhibited_page.exhibited_sections.last.move_to_top
         flash[:notice] = 'A new Exhibited Section was successfully added.'
-        format.html { redirect_to edit_exhibit_url(:id => @exhibit) }
+        format.html { redirect_to edit_page_path(:id => @exhibited_page, :exhibit_id => @exhibited_page.exhibit_id) }
         format.xml  { head :ok }
       else
         format.html do
           flash[:error] = "There was a problem creating a new Exibited Section."
-          redirect_to edit_exhibit_url(@exhibit)
+          redirect_to edit_page_path(:id => @exhibited_page, :exhibit_id => @exhibited_page.exhibit_id)
         end
         format.xml  { render :xml => @exhibited_section.errors.to_xml }
       end
@@ -49,29 +49,29 @@ class ExhibitedSectionsController < ExhibitsBaseController
   end
   
   def update
-    @exhibited_section = ExhibitedSection.find(params[:id])
-    @exhibit = Exhibit.find(params[:exhibit_id])
+    @exhibited_page = @exhibit.exhibited_pages.find(params[:page_id])
+    @exhibited_section = @exhibited_page.exhibited_sections.find(params[:id])
 
     respond_to do |format|
       if @exhibited_section.update_attributes(params[:exhibited_section])
         flash[:notice] = 'Exhibited Section was successfully updated.'
-        format.html { redirect_to edit_exhibit_url(:id => @exhibit, :anchor => dom_id(@exhibited_section)) }
+        format.html { redirect_to edit_page_path(:id => @exhibited_page, :exhibit_id => @exhibited_page.exhibit_id, :anchor => dom_id(@exhibited_section)) }
         format.xml  { head :ok }
       else
-        format.html { redirect_to edit_exhibit_url(@exhibit) }
+        format.html { redirect_to edit_page_path(:id => @exhibited_page, :exhibit_id => @exhibited_page.exhibit_id) }
         format.xml  { render :xml => @exhibited_section.errors.to_xml }
       end
     end
   end
   
   def destroy
-    @exhibited_section = ExhibitedSection.find(params[:id])
-    @exhibit = Exhibit.find(params[:exhibit_id])
+    @exhibited_page = @exhibit.exhibited_pages.find(params[:page_id])
+    @exhibited_section = @exhibited_page.exhibited_sections.find(params[:id])
     @exhibited_section.destroy
 
     respond_to do |format|
       flash[:notice] = 'Exhibited Section was successfully removed.'
-      format.html { redirect_to edit_exhibit_url(@exhibit) }
+      format.html { redirect_to edit_page_path(:id => @exhibited_page, :exhibit_id => @exhibited_page.exhibit_id) }
       format.xml  { head :ok }
     end
   end
