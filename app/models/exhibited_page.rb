222f34829f3e18193284a25a290062714f0699be
Jamie Orchard-Hays <jamieorc@gmail.com>
2007-06-18 23:14:52 +0000

    aliased exhibited_sections as sections and added sections_full?() to indicated if page is already full

--- app/models/exhibited_page.rb
+++ app/models/exhibited_page.rb
@@ -1,11 +1,16 @@
 class ExhibitedPage < ActiveRecord::Base
   has_many :exhibited_sections, :order => "position", :dependent => :destroy
+  alias_method :sections, :exhibited_sections
   belongs_to :exhibit_page_type
   belongs_to :exhibit
   acts_as_list :scope => :exhibit
   
   def template
     self.exhibit_page_type.template
+  end
+  
+  def sections_full?
+    sections.count >= exhibit_page_type.max_sections
   end
   
 end
