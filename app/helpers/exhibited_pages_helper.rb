893dc99a517c8ce2274d1e8c4f2226b23b5938bc
Jamie Orchard-Hays <jamieorc@gmail.com>
2007-06-18 18:26:45 +0000

    exhibited_pages_links generates links for an exhibit's pages

--- app/helpers/exhibited_pages_helper.rb
+++ app/helpers/exhibited_pages_helper.rb
@@ -1,2 +1,16 @@
 module ExhibitedPagesHelper
+  def exhibited_pages_links(exhibited_page, edit = false)
+    exhibit = exhibited_page.exhibit
+    position = exhibited_page.position
+    index = position - 1
+    max_index = exhibit.pages.size - 1
+    
+    route_method = edit ? :edit_page_path : :page_path
+    puts "route_method: #{route_method}"
+    links = ""
+    links << link_to(h("<"), __send__(route_method, :id => exhibit.pages[index - 1].id, :exhibit_id => exhibit.id)) + "&nbsp;" unless index < 1
+    links << "#{position}"
+    links << "&nbsp;" + link_to(h(">"), __send__(route_method, :id => exhibit.pages[index + 1].id, :exhibit_id => exhibit.id)) unless index >= max_index
+    links
+  end
 end
