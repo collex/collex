bcf8ecdb83049c385003035e8b758847591b216c
Jamie Orchard-Hays <jamieorc@gmail.com>
2007-06-19 19:42:44 +0000

    aliased section to exhibited_section

--- app/models/exhibited_resource.rb
+++ app/models/exhibited_resource.rb
@@ -1,6 +1,7 @@
 class ExhibitedResource < ActiveRecord::Base
 #   belongs_to :resource, :class_name => "SolrResource"
   belongs_to :exhibited_section
+  alias_method :section, :exhibited_section
   acts_as_list :scope => :exhibited_section
   
   def resource
