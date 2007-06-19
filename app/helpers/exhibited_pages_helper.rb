63de0468501254371bafd5dfbc016ef32305f7e9
Jamie Orchard-Hays <jamieorc@gmail.com>
2007-06-19 22:33:08 +0000

    show first and/or last page number unless on one of those pages

--- app/helpers/exhibited_pages_helper.rb
+++ app/helpers/exhibited_pages_helper.rb
@@ -8,9 +8,11 @@ module ExhibitedPagesHelper
     route_method = edit ? :edit_page_path : :page_path
     puts "route_method: #{route_method}"
     links = ""
-    links << link_to(h("<"), __send__(route_method, :id => exhibit.pages[index - 1].id, :exhibit_id => exhibit.id)) + "&nbsp;" unless index < 1
+    links << link_to("1&nbsp;", __send__(route_method, exhibit, exhibit.pages.first)) unless index < 1
+    links << link_to(h("<"), __send__(route_method, exhibit, exhibit.pages[index - 1])) + "&nbsp;" unless index < 1
     links << "#{position}"
-    links << "&nbsp;" + link_to(h(">"), __send__(route_method, :id => exhibit.pages[index + 1].id, :exhibit_id => exhibit.id)) unless index >= max_index
+    links << "&nbsp;" + link_to(h(">"), __send__(route_method, exhibit, exhibit.pages[index + 1])) unless index >= max_index
+    links << "&nbsp;" + link_to(exhibit.pages.count, __send__(route_method, exhibit, exhibit.pages[-1])) unless index >= max_index
     links
   end
 end
