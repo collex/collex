ec71cb9fa27f409f4006be57f74aeff9eea7ff55
Jamie Orchard-Hays <jamieorc@gmail.com>
2007-06-26 22:53:10 +0000

    the move_* operations have been moved to ExhibitedItemsController. Also, added ability to just add ExhibitedText, but this is temporary code.

--- app/controllers/exhibited_resources_controller.rb
+++ app/controllers/exhibited_resources_controller.rb
@@ -1,73 +1,24 @@
-class ExhibitedResourcesController < ExhibitsBaseController
+class ExhibitedResourcesController < ExhibitedItemsController
   
   in_place_edit_for_resource :exhibited_resource, :annotation
 
-  def index
-    @exhibited_resources = ExhibitedResource.find(:all)
-
-    respond_to do |format|
-      format.html # index.rhtml
-      format.xml  { render :xml => @exhibited_resources.to_xml }
-    end
-  end
-  
-  def move_higher
-    move_item(:move_higher, "Moved Exhibited Resource Up.")
-  end  
-  def move_lower
-    move_item(:move_lower, "Moved Exhibited Resource Down.")
-  end  
-  def move_to_top
-    move_item(:move_to_top, "Moved Exhibited Resource to Top.")
-  end  
-  def move_to_bottom
-    move_item(:move_to_bottom, "Moved Exhibited Resource to Bottom.")
-  end
-  def move_item(command, notice)
-    @exhibited_resource = ExhibitedResource.find(params[:id])
-    @exhibited_resource.__send__(command)
-    logger.info("ExhibitedResource: #{command.to_s}: #{params[:id]}")
-    flash[:notice] = notice
-    redirect_to edit_page_path(:exhibit_id => params[:exhibit_id], :id => params[:page_id], :anchor => dom_id(@exhibited_resource))
-  rescue Exception
-    logger.info("Error: #{command} with id=#{params[:id]} failed.")
-    flash[:error] = "There was an error moving your resource."
-    redirect_to edit_page_path(:exhibit_id => params[:exhibit_id], :id => params[:page_id])
-  end
-  private :move_item
-
-  def show
-    @exhibited_resource = ExhibitedResource.find(params[:id])
-
-    respond_to do |format|
-      format.html # show.rhtml
-      format.xml  { render :xml => @exhibited_resource.to_xml }
-    end
-  end
-
-  def new
-    @exhibited_resource = ExhibitedResource.new
-  end
-
-  def edit
-    @exhibited_resource = ExhibitedResource.find(params[:id])
-  end
-
   def create
     @exhibited_resource = ExhibitedResource.new(params[:exhibited_resource])
+    exhibited_section_id = params[:exhibited_section_id].to_i
+    es = ExhibitedSection.find(exhibited_section_id)
 
-    unless params[:new_resource].blank?
-      uri = params[:new_resource].match('thumbnail_').post_match
-      exhibited_section_id = params[:exhibited_section_id].to_i
-      es = ExhibitedSection.find(exhibited_section_id)
-      er = ExhibitedResource.new(:uri => uri)
-      es.exhibited_resources << er
-      es.exhibited_resources.last.move_to_top
-      flash[:error] = "You now have a duplicate of that object in your collection." if es.page.exhibit.uris.include?(uri)
-      flash[:notice] = "The Resource was successfully added."
+    if params[:new_resource].blank?
+      er = ExhibitedResource.new
+      flash[:notice] = "The Text area was successfully created."
+      #flash[:error] = "You now have a duplicate of that object in your collection." if es.page.exhibit.uris.include?(uri)
     else
-      flash[:error] = "No Resource could be added."
-    end
+      uri = params[:new_resource].match('thumbnail_').post_match
+      er = ExhibitedResource.new(:uri => uri)
+      flash[:notice] = "The Resource was successfully added."
+      # flash[:error] = "No Resource could be added."
+    end  
+    es.exhibited_resources << er
+    es.exhibited_resources.last.move_to_top
     unless er.blank?
       redirect_to edit_page_url(:exhibit_id => es.page.exhibit, :id => es.page, :anchor => dom_id(er))
     else
