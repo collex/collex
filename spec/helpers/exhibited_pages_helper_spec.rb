903f1c3f30c6772dcbdada374d17c4f979352415
Jamie Orchard-Hays <jamieorc@gmail.com>
2007-06-26 20:55:52 +0000

    updated spec for current version of helper (CC.rb must not be running the specs because it didn't report failures)

--- spec/helpers/exhibited_pages_helper_spec.rb
+++ spec/helpers/exhibited_pages_helper_spec.rb
@@ -9,30 +9,28 @@ describe ExhibitedPagesHelper do
   end
   
   it "'exhibited_pages_links' should return show links in non-edit mode and edit links in edit mode" do
-    page_1 = mock("page_1", :null_object => true)
-    page_2 = mock("page_2", :null_object => true)
-    page_3 = mock("page_3", :null_object => true)
+    page_1 = mock_model(ExhibitedPage, :null_object => true)
+    page_2 = mock_model(ExhibitedPage, :null_object => true)
+    page_3 = mock_model(ExhibitedPage, :null_object => true)
     pages = [page_1, page_2, page_3]
     
-    exhibit = mock("exhibit")
-    exhibit.stub!(:id).and_return(1)
+    exhibit = mock_model(Exhibit)
     exhibit.stub!(:pages).and_return(pages)
     pages.each_with_index do |page, index|
-      page.stub!(:id).and_return(index + 1)
       page.stub!(:position).and_return(index + 1)
       page.stub!(:exhibit).and_return(exhibit)
     end
 #     puts h exhibited_pages_links(page_3)
 #     puts exhibited_pages_links(page_2)
     # non-edit mode
-    exhibited_pages_links(page_1).should == %q{1&nbsp;<a href="/exhibits/1/pages/2">&gt;</a>}
-    exhibited_pages_links(page_2).should == %q{<a href="/exhibits/1/pages/1">&lt;</a>&nbsp;2&nbsp;<a href="/exhibits/1/pages/3">&gt;</a>}
-    exhibited_pages_links(page_3).should == %q{<a href="/exhibits/1/pages/2">&lt;</a>&nbsp;3}
+    exhibited_pages_links(page_1).should == "1&nbsp;" + link_to("&gt;", page_path(exhibit, page_2)) + "&nbsp;" + link_to("3", page_path(exhibit, page_3))
+    exhibited_pages_links(page_2).should == link_to("1&nbsp;", page_path(exhibit, page_1)) + link_to("&lt;", page_path(exhibit, page_1)) + "&nbsp;2&nbsp;" + link_to("&gt;", page_path(exhibit, page_3)) + "&nbsp;" + link_to("3", page_path(exhibit, page_3))
+    exhibited_pages_links(page_3).should == link_to("1&nbsp;", page_path(exhibit, page_1)) + link_to("&lt;", page_path(exhibit, page_2)) + "&nbsp;3"
     
     # edit mode
-    exhibited_pages_links(page_1, true).should == %q{1&nbsp;<a href="/exhibits/1/pages/2;edit">&gt;</a>}
-    exhibited_pages_links(page_2, true).should == %q{<a href="/exhibits/1/pages/1;edit">&lt;</a>&nbsp;2&nbsp;<a href="/exhibits/1/pages/3;edit">&gt;</a>}
-    exhibited_pages_links(page_3, true).should == %q{<a href="/exhibits/1/pages/2;edit">&lt;</a>&nbsp;3}
+    exhibited_pages_links(page_1, true).should == "1&nbsp;" + link_to("&gt;", edit_page_path(exhibit, page_2)) + "&nbsp;" + link_to("3", edit_page_path(exhibit, page_3))
+    exhibited_pages_links(page_2, true).should == link_to("1&nbsp;", edit_page_path(exhibit, page_1)) + link_to("&lt;", edit_page_path(exhibit, page_1)) + "&nbsp;2&nbsp;" + link_to("&gt;", edit_page_path(exhibit, page_3)) + "&nbsp;" + link_to("3", edit_page_path(exhibit, page_3))
+    exhibited_pages_links(page_3, true).should == link_to("1&nbsp;", edit_page_path(exhibit, page_1)) + link_to("&lt;", edit_page_path(exhibit, page_2)) + "&nbsp;3"
     
   end
 end
