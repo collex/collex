c4129712e1f96ad356b068cbbe39f0b3efa2de04
Jamie Orchard-Hays <jamieorc@gmail.com>
2007-06-26 23:45:51 +0000

    with new superclass and routing structure, ExhibitedResourcesController only needs a create method. This is the previous version of the method.

--- app/controllers/exhibited_resources_controller.rb
+++ app/controllers/exhibited_resources_controller.rb
@@ -4,21 +4,19 @@ class ExhibitedResourcesController < ExhibitedItemsController
 
   def create
     @exhibited_resource = ExhibitedResource.new(params[:exhibited_resource])
-    exhibited_section_id = params[:exhibited_section_id].to_i
-    es = ExhibitedSection.find(exhibited_section_id)
 
-    if params[:new_resource].blank?
-      er = ExhibitedResource.new
-      flash[:notice] = "The Text area was successfully created."
-      #flash[:error] = "You now have a duplicate of that object in your collection." if es.page.exhibit.uris.include?(uri)
-    else
+    unless params[:new_resource].blank?
       uri = params[:new_resource].match('thumbnail_').post_match
+      exhibited_section_id = params[:exhibited_section_id].to_i
+      es = ExhibitedSection.find(exhibited_section_id)
       er = ExhibitedResource.new(:uri => uri)
+      es.exhibited_resources << er
+      es.exhibited_resources.last.move_to_top
+      flash[:error] = "You now have a duplicate of that object in your collection." if es.page.exhibit.uris.include?(uri)
       flash[:notice] = "The Resource was successfully added."
-      # flash[:error] = "No Resource could be added."
-    end  
-    es.exhibited_resources << er
-    es.exhibited_resources.last.move_to_top
+    else
+      flash[:error] = "Resource was not added."
+    end
     unless er.blank?
       redirect_to edit_page_url(:exhibit_id => es.page.exhibit, :id => es.page, :anchor => dom_id(er))
     else
@@ -26,32 +24,4 @@ class ExhibitedResourcesController < ExhibitedItemsController
     end  
   end
 
-  def update
-    @exhibited_resource = ExhibitedResource.find(params[:id])
-    @exhibit = Exhibit.find(params[:exhibit_id])
-
-    respond_to do |format|
-      if @exhibited_resource.update_attributes(params[:exhibited_resource])
-        flash[:notice] = 'Exhibited Resource was successfully updated.'
-        format.html { redirect_to edit_exhibit_url(:id => @exhibit, :anchor => dom_id(@exhibited_resource)) }
-        format.xml  { head :ok }
-      else
-        format.html { redirect_to edit_exhibit_url(@exhibit) }
-        format.xml  { render :xml => @exhibited_resource.errors.to_xml }
-      end
-    end
-  end
-
-  def destroy
-    @exhibited_resource = ExhibitedResource.find(params[:id])
-    @exhibit = Exhibit.find(params[:exhibit_id])
-    @exhibited_resource.destroy
-
-    respond_to do |format|
-      flash[:notice] = 'Exhibited Resource was successfully removed.'
-      page = params[:page] || 1
-      format.html { redirect_to edit_exhibit_url(:id => @exhibit, :page => page) }
-      format.xml  { head :ok }
-    end
-  end
 end
