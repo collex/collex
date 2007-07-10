c1f9c1c4714d24637a21787ff21cf42debe5eb80
Jamie Orchard-Hays <jamieorc@gmail.com>
2007-07-10 20:50:08 +0000

    added save plugin to tinyMCE and configuration for it. Also set tinyMCE's encoding to "raw", which only encodes " ' < and >

--- app/controllers/exhibited_pages_controller.rb
+++ app/controllers/exhibited_pages_controller.rb
@@ -8,10 +8,12 @@ class ExhibitedPagesController < ExhibitsBaseController
                 :theme_advanced_toolbar_location => "top",
                 :theme_advanced_toolbar_align => "left",
                 :theme_advanced_resizing => true,
-                :theme_advanced_buttons1 => "bold,italic,underline,separator,preview,separator,outdent,indent,unlink,link,separator,undo,redo,separator,cleanup,code,help",
+                :theme_advanced_buttons1 => "bold,italic,underline,separator,preview,separator,outdent,indent,unlink,link,separator,undo,redo,separator,cleanup,code,help,save,cancel",
                 :theme_advanced_buttons2 => "",
                 :theme_advanced_buttons3 => "",
-                :plugins => ["preview"]},
+                :plugins => ["preview", "save"],
+                :save_enablewhendirty => true,
+                :entity_encoding => "raw"},
                 :only => [:edit])
   
   in_place_edit_for_resource :exhibited_page, :title
