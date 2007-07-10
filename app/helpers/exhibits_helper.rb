01e04678bf0f4f921a137cf14ecf001300635aff
Jamie Orchard-Hays <jamieorc@gmail.com>
2007-07-10 20:44:10 +0000

    don't escape annotation text (use << with Builder); added highlight for text, similar to in_place_editor(), as well as the "Click to edit" tool tip. This now functions somewhat similarly to in_place_editor, but using tinyMCE. There needs to be some more development around error handling, which there's none right now.

--- app/helpers/exhibits_helper.rb
+++ app/helpers/exhibits_helper.rb
@@ -111,19 +111,19 @@ module ExhibitsHelper
   def exhibited_resource_in_place_editor_area(object, method, tag_options = {})
     tag = ::ActionView::Helpers::InstanceTag.new(object, method, self)
     update_id = tag_options[:update_id]
+    mce_id = "mce_#{object}_#{tag.object.id}"
+    text_id = "text_#{object}_#{tag.object.id}"
     value = tag.object.__send__(method) || tag_options[:value] || "(No annotation given)"
     url = eval("update_#{method}_#{object}_path(#{tag.object.exhibited_section.exhibited_page.exhibit.id}, #{tag.object.exhibited_section.exhibited_page.id}, #{tag.object.exhibited_section.id}, #{tag.object.id})")
     html = Builder::XmlMarkup.new(:indent => 2)
-    html.span(value, :id => "text_#{object}_#{tag.object.id}", :onclick => "Element.show('mce_#{object}_#{tag.object.id}'); Element.hide('text_#{object}_#{tag.object.id}')")
-    html.span(:style => "display: none;", :id => "mce_#{object}_#{tag.object.id}") do
-      html << form_remote_tag(:url => url, :class => "tiny-mce-on")
-      html << text_area_tag(:value, value, :class => "tiny-mce")
+    html << span(value, :id => text_id, :onclick => "Element.show('#{mce_id}'); Element.hide('#{text_id}')", :onmouseover => "this.style.backgroundColor = '#ffff99'; this.title = 'Click to edit';", :onmouseout => "this.style.backgroundColor = '#ffffff';")
+    html.span(:style => "display: none;", :id => mce_id) do
+      html << form_remote_tag(:url => url, :class => "tiny-mce-on", :update => text_id, :complete => "Element.hide('#{mce_id}'); Element.show('#{text_id}')")
+      html << text_area_tag(:value, value, :id => "text_area_#{object}_#{tag.object.id}", :class => "tiny-mce")
       html << hidden_field_tag(:update_id, update_id)
-      html.input(:type => "submit", :value => "Save")
-      html << link_to_function("cancel", "Element.hide('mce_#{object}_#{tag.object.id}'); Element.show('text_#{object}_#{tag.object.id}')")
+      html << link_to_function("cancel", "Element.hide('#{mce_id}'); Element.show('#{text_id}')")
     end
     html << "</form>"
-#     render :partial => "tiny_mce_editor", :locals => {:url => url, :object => object, :method => method, :ojbect_id => tag.object.id}
     html.to_s
   end
 end
