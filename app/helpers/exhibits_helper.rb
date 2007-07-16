844c70347bfff848ed8ee3f43ea16bbcc550e479
Jamie Orchard-Hays <jamieorc@gmail.com>
2007-07-16 19:55:59 +0000

    Give message about rich text editing browsers if using Safair

--- app/helpers/exhibits_helper.rb
+++ app/helpers/exhibits_helper.rb
@@ -127,7 +127,8 @@ module ExhibitsHelper
     
     html = Builder::XmlMarkup.new(:indent => 2)
     html << span(value, :id => text_id, :onclick => "this.style.backgroundColor = '#ffffff';Element.show('#{mce_id}'); Element.hide('#{text_id}')", :onmouseover => "this.style.backgroundColor = '#ffff99'; this.title = 'Click to edit';", :onmouseout => "this.style.backgroundColor = '#ffffff';")
-    html.span(:style => "display: none;", :id => mce_id) do
+    html.div(:style => "display: none;", :id => mce_id) do
+      html.p("Rich text editing is not supported in Safari. For rich text editing, we recommend a gecko-based browser such as FireFox or Camino.") if request.env['HTTP_USER_AGENT'].downcase.index('safari') != nil
       html << link_to_function("cancel", "Element.hide('#{mce_id}'); Element.show('#{text_id}')")
       html << form_remote_tag(:url => url, :class => "tiny-mce-on", :update => text_id, :complete => "Element.hide('#{mce_id}'); Element.show('#{text_id}')")
       html << text_area_tag(:value, value, :id => "text_area_#{object}_#{tag.object.id}", :class => "tiny-mce")
