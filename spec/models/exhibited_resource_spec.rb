276d5b195f443832207bec13eaa35f8245c15c56
Jamie Orchard-Hays <jamieorc@gmail.com>
2007-08-01 20:46:51 +0000

    place holder for ExhibitedResource spec

---
+++ spec/models/exhibited_resource_spec.rb
@@ -0,0 +1,11 @@
+require File.dirname(__FILE__) + '/../spec_helper'
+
+describe ExhibitedResource do
+  before(:each) do
+    @er = mock_model(ExhibitedResource)
+  end
+
+  it "'date_label_or_date' should return date_label if present, date otherwise" do
+    
+  end
+end
