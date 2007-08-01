f629baeeb8d23f0fc46b5f0378acb95c5e56216b
Jamie Orchard-Hays <jamieorc@gmail.com>
2007-08-01 20:22:43 +0000

    empty spec for Exhibit model. Need to create specs for roles and authorization

---
+++ spec/models/exhibit_spec.rb
@@ -0,0 +1,9 @@
+require File.dirname(__FILE__) + '/../spec_helper'
+
+describe Exhibit do
+  fixtures :exhibits, :exhibited_pages, :exhibit_page_types, :exhibited_sections
+
+  it "should test authorization/roles for Exhibit" do
+    
+  end
+end
