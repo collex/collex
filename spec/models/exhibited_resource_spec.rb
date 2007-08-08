3c82cec8f32b5b590ea82a97ae14dcc627e94212
Jamie Orchard-Hays <jamieorc@gmail.com>
2007-08-08 16:22:04 +0000

    spec for empty resource returning empty SolrResouce rather than nil

--- spec/models/exhibited_resource_spec.rb
+++ spec/models/exhibited_resource_spec.rb
@@ -8,4 +8,9 @@ describe ExhibitedResource do
   it "'date_label_or_date' should return date_label if present, date otherwise" do
     
   end
+  
+  it "'resource' should return empty SolrResource if Solr returns null for the id" do
+    er = ExhibitedResource.new
+    er.resource.class.should equal(SolrResource)
+  end
 end
