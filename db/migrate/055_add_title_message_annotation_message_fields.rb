94f54d1f55782fd4a437483bc1b9d89704a37c29
Jamie Orchard-Hays <jamieorc@gmail.com>
2007-08-09 21:22:47 +0000

    rescue updates b/c test db doesn't have these!

--- db/migrate/055_add_title_message_annotation_message_fields.rb
+++ db/migrate/055_add_title_message_annotation_message_fields.rb
@@ -17,22 +17,22 @@ class AddTitleMessageAnnotationMessageFields < ActiveRecord::Migration
     add_column :exhibit_section_types, :annotation_message, :string
     
     ann_bib = ExhibitType.find_by_template "annotated_bibliography"
-    ExhibitType.update ann_bib.id, :title_message => "(Insert Bibliography Title)", :annotation_message => "(Insert General Description)"
+    ExhibitType.update ann_bib.id, :title_message => "(Insert Bibliography Title)", :annotation_message => "(Insert General Description)" rescue nil
 
     ill_ess = ExhibitType.find_by_template "illustrated_essay"
-    ExhibitType.update ill_ess.id, :title_message => "(Insert Essay Title)", :annotation_message => "(Insert Abstract)"
+    ExhibitType.update ill_ess.id, :title_message => "(Insert Essay Title)", :annotation_message => "(Insert Abstract)" rescue nil
     
     ann_bib_page = ExhibitPageType.find_by_name "Annotated Bibliography Page Type"
-    ExhibitPageType.update ann_bib_page.id, :title_message => "(Insert Page Title)", :annotation_message => "(Insert Page Notes)"
+    ExhibitPageType.update ann_bib_page.id, :title_message => "(Insert Page Title)", :annotation_message => "(Insert Page Notes)" rescue nil
 
     ill_ess_page = ExhibitPageType.find_by_name "Illustrated Essay Page Type"
-    ExhibitPageType.update ill_ess_page.id, :title_message => "(Insert Page Title)", :annotation_message => "(Insert Page Notes)"
+    ExhibitPageType.update ill_ess_page.id, :title_message => "(Insert Page Title)", :annotation_message => "(Insert Page Notes)" rescue nil
     
     ann_bib_section = ExhibitSectionType.find_by_template "citation"
-    ExhibitSectionType.update ann_bib_section.id, :title_message => "(Insert Section Title)", :annotation_message => "(Insert Section Description)"
+    ExhibitSectionType.update ann_bib_section.id, :title_message => "(Insert Section Title)", :annotation_message => "(Insert Section Description)" rescue nil
 
     ill_ess_section = ExhibitSectionType.find_by_template "ie_generic"
-    ExhibitSectionType.update ill_ess_section.id, :title_message => "(Insert Section Title)", :annotation_message => "(Insert Text)"
+    ExhibitSectionType.update ill_ess_section.id, :title_message => "(Insert Section Title)", :annotation_message => "(Insert Text)" rescue nil
   end
 
   def self.down
