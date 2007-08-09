722a350485f27920e69bd3fe0d66693c2865fc44
Jamie Orchard-Hays <jamieorc@gmail.com>
2007-08-09 20:38:14 +0000

    added output of user's roles for logging when not authorized as owner

--- app/controllers/exhibits_base_controller.rb
+++ app/controllers/exhibits_base_controller.rb
@@ -8,7 +8,7 @@ class ExhibitsBaseController < ApplicationController
       id = params[:exhibit_id] || params[:id]
       @exhibit = Exhibit.find(id)
       unless @exhibit.updatable_by?(user_or_guest)
-        logger.info("#{user_or_guest.username} is not owner of #{@exhibit.title} (id=#{@exhibit.id})")
+        logger.info("#{user_or_guest.username} with roles #{user_or_guest.role_names.join(', ')} may not edit #{@exhibit.title} (id=#{@exhibit.id})")
         flash[:warning] = "You do not have permission to edit that Exhibit!"
         redirect_to(exhibits_path) and return false
       end
