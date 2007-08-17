e347841f55bf484b81d5fceaad9d40d30139a880
Jamie Orchard-Hays <jamieorc@gmail.com>
2007-08-17 20:53:15 +0000

    started to rework the pagination design--not finished yet

--- app/helpers/exhibited_pages_helper.rb
+++ app/helpers/exhibited_pages_helper.rb
@@ -7,11 +7,12 @@ module ExhibitedPagesHelper
     
     route_method = edit ? :edit_page_path : :page_path
     links = ""
-    links << link_to("1&nbsp;", __send__(route_method, exhibit, exhibit.pages.first)) unless index < 1
-    links << link_to(h("<"), __send__(route_method, exhibit, exhibit.pages[index - 1])) + "&nbsp;" unless index < 1
+    links << link_to("&lt;&lt;&nbsp;first&nbsp;", __send__(route_method, exhibit, exhibit.pages.first)) unless index < 1
+    links << link_to("&lt;&nbsp;prev", __send__(route_method, exhibit, exhibit.pages[index - 1])) + "&nbsp;" unless index < 1
     links << "#{position}"
-    links << "&nbsp;" + link_to(h(">"), __send__(route_method, exhibit, exhibit.pages[index + 1])) unless index >= max_index
-    links << "&nbsp;" + link_to(exhibit.pages.size, __send__(route_method, exhibit, exhibit.pages[-1])) unless index >= max_index
+    links << "&nbsp;" + link_to("next&nbsp;&gt;", __send__(route_method, exhibit, exhibit.pages[index + 1])) unless index >= max_index
+#     links << "&nbsp;" + link_to(exhibit.pages.size, __send__(route_method, exhibit, exhibit.pages[-1])) unless index >= max_index
+    links << link_to("&nbsp;&nbsp;last&nbsp;&gt;&gt;", __send__(route_method, exhibit, exhibit.pages[-1])) unless index >= max_index
     links
   end
 end
