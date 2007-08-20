d8b355a8601f0af5d3efe6ceef4b4f701751da02
Jamie Orchard-Hays <jamieorc@gmail.com>
2007-08-20 14:42:42 +0000

    added page number to last in exhibited_page_links

--- app/helpers/exhibited_pages_helper.rb
+++ app/helpers/exhibited_pages_helper.rb
@@ -12,7 +12,7 @@ module ExhibitedPagesHelper
     links << "#{position}"
     links << "&nbsp;" + link_to("next&nbsp;&gt;", __send__(route_method, exhibit, exhibit.pages[index + 1])) unless index >= max_index
 #     links << "&nbsp;" + link_to(exhibit.pages.size, __send__(route_method, exhibit, exhibit.pages[-1])) unless index >= max_index
-    links << link_to("&nbsp;&nbsp;last&nbsp;&gt;&gt;", __send__(route_method, exhibit, exhibit.pages[-1])) unless index >= max_index
+    links << link_to("&nbsp;&nbsp;last&nbsp;(#{exhibit.pages.size})&nbsp;&gt;&gt;", __send__(route_method, exhibit, exhibit.pages[-1])) unless index >= max_index
     links
   end
 end
