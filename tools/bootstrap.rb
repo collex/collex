84eb76c37b95fcd8220cead750a2a234a90d971d
Erik Hatcher <erik.hatcher@gmail.com>
2007-08-27 12:47:25 +0000

    remove old Kowari related scripts

--- tools/bootstrap.rb
+++
@@ -1,9 +0,0 @@
-# Execute Kowari scripts to create a new DB
-# create <rmi://localhost/server1#collex>;
-# create <rmi://localhost/server1#nines>;
-# create <rmi://localhost/server1#user>;
-# create <rmi://nines.org/server1#xsd> <http://tucana.org/tucana#XMLSchemaModel>;
-
-
-# insert a basic user/password
-
