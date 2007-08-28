60fc2734fbc937f5dd6c06dab99d648e826946bd
Jamie Orchard-Hays <jamieorc@gmail.com>
2007-08-28 17:32:34 +0000

    fixed weird spec error that was caused by using a constant here, but through the error in exhibited_resource_spec

--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -2,9 +2,9 @@ require File.dirname(__FILE__) + '/../spec_helper'
 
 
 describe ApplicationHelper do
-  URI = 'http://test/uri'
   before(:each) do
-    @item = {'uri' => URI}
+    @uri = 'http://test/uri'
+    @item = {'uri' => @uri}
     @er = mock_model(ExhibitedResource)
     @generic_site = mock_model(Site, :thumbnail => '')
     @specific_site = mock_model(Site, :thumbnail => 'http://some.site.url.com/image.gif', :description => 'Site Description')
@@ -14,7 +14,7 @@ describe ApplicationHelper do
   it "'thumbnail_image' shoud get generic image when no others" do  
     Site.should_receive(:find_by_code).with('generic').and_return(@generic_site)
     item = @item.merge({'archive' => 'generic', 'title' => 'The Generic'})
-    expected = %(<img src="#{DEFAULT_THUMBNAIL_IMAGE_PATH}" alt="The Generic" align="left" id="thumbnail_#{URI}"/>)
+    expected = %(<img src="#{DEFAULT_THUMBNAIL_IMAGE_PATH}" alt="The Generic" align="left" id="thumbnail_#{@uri}"/>)
     result = thumbnail_image_tag(item)
     assert_dom_equal(expected, result)
   end
@@ -22,7 +22,7 @@ describe ApplicationHelper do
   it "thumbnail_image_tag should get site image when no specific thumbnail" do
     Site.should_receive(:find_by_code).with('site').and_return(@specific_site)
     item = @item.merge({'archive' => 'site', 'title' => 'Specific Site Title'})
-    expected = %(<img src="http://some.site.url.com/image.gif" alt="Specific Site Title" align="left" id="thumbnail_#{URI}"/>)
+    expected = %(<img src="http://some.site.url.com/image.gif" alt="Specific Site Title" align="left" id="thumbnail_#{@uri}"/>)
     result = thumbnail_image_tag(item)
     assert_dom_equal(expected, result)
   end
@@ -30,7 +30,7 @@ describe ApplicationHelper do
   it "'thumbnail_image_tag' should get thumbnail of specific item when present" do
     Site.should_receive(:find_by_code).with('site').and_return(@specific_site)
     item = @item.merge({'archive' => 'site', 'title' => 'Specific Site Title', 'thumbnail' => 'http://some.specific.url.com/image.gif'})
-    expected = %(<img src="http://some.specific.url.com/image.gif" alt="Specific Site Title" align="left" id="thumbnail_#{URI}"/>)
+    expected = %(<img src="http://some.specific.url.com/image.gif" alt="Specific Site Title" align="left" id="thumbnail_#{@uri}"/>)
     result = thumbnail_image_tag(item)
     assert_dom_equal(expected, result)
   end
@@ -38,7 +38,7 @@ describe ApplicationHelper do
   it "'thumbnail_image_tag' without extension" do
     Site.should_receive(:find_by_code).with('site').and_return(@specific_site)
     item = @item.merge({'archive' => 'site', 'title' => 'Specific Site Title', 'thumbnail' => 'http://www.purl.org/swinburnearchive/img/tsa9thmb00/'})
-    expected = %(<img src="http://www.purl.org/swinburnearchive/img/tsa9thmb00/" alt="Specific Site Title" align="left" id="thumbnail_#{URI}"/>)
+    expected = %(<img src="http://www.purl.org/swinburnearchive/img/tsa9thmb00/" alt="Specific Site Title" align="left" id="thumbnail_#{@uri}"/>)
     result = thumbnail_image_tag(item)
     assert_dom_equal(expected, result)
   end
