af5dd6d1c5f7034fb11f4660a8a9ab29d546727b
Jamie Orchard-Hays <jamieorc@gmail.com>
2007-09-19 17:47:09 +0000

    added a bunch of specs for the signup process regarding username, email, password, duplicate usernames

--- spec/controllers/login_controller_spec.rb
+++ spec/controllers/login_controller_spec.rb
@@ -27,4 +27,46 @@ describe LoginController do
     get :logout
     response.should redirect_to(:controller => "search", :action => "browse")
   end
+  
+  it "'signup' should accept usernames with periods" do
+    post :signup, {:username => 'user.name', :email => "fake@email.com", :fullname => "Full Name", :password => "password", :password2 => "password"}
+    response.should redirect_to(:controller => "sidebar", :action => "cloud", :type => "genre")
+    session[:user][:username].should == "user.name"
+  end
+  
+  it "'signup' should reject usernames with spaces" do
+    post :signup, {:username => 'user name', :email => "fake@email.com", :fullname => "Full Name", :password => "password", :password2 => "password"}
+    response.should be_success
+    response.should render_template("signup")
+  end
+  
+  it "'signup' should reject email addresses without '@'" do
+    post :signup, {:username => 'username', :email => "fakeemail.com", :fullname => "Full Name", :password => "password", :password2 => "password"}
+    response.should be_success
+    response.should render_template("signup")
+  end
+  
+  it "'signup' should not allow blank passwords" do
+    post :signup, {:username => 'username', :email => "fake@email.com", :fullname => "Full Name", :password => "", :password2 => ""}
+    response.should be_success
+    response.should render_template("signup")
+  end
+  
+  it "'signup' should not allow non-matching passwords" do
+    post :signup, {:username => 'username', :email => "fake@email.com", :fullname => "Full Name", :password => "one", :password2 => "two"}
+    response.should be_success
+    response.should render_template("signup")
+  end
+  
+  it "'signup' should not allow duplicate usernames" do
+    post :signup, {:username => 'username', :email => "fake@email.com", :fullname => "Full Name", :password => "password", :password2 => "password"}
+    response.should be_redirect
+    response.should redirect_to(:controller => "sidebar", :action => "cloud", :type => "genre")
+    session[:user][:username].should == "username"
+
+    post :signup, {:username => 'username', :email => "fakeemail.com", :fullname => "Full Name", :password => "password", :password2 => "password"}
+    response.should be_success
+    response.should render_template("signup")
+  end
+  
 end
