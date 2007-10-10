33cbc08a7938a70ccb633b103f95914dfc65fef4
Jamie Orchard-Hays <jamieorc@gmail.com>
2007-10-10 16:53:10 +0000

    Added Apache 2.0 license to all .rb files. Other files next.

--- app/models/saved_search_constraint.rb
+++ app/models/saved_search_constraint.rb
@@ -1,3 +1,17 @@
+##########################################################################
+# The ASF licenses this file to You under the Apache License, Version 2.0
+# (the "License"); you may not use this file except in compliance with
+# the License.  You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+##########################################################################
+
 class SavedSearchConstraint < Constraint
   def to_solr_expression
     User.find_by_username(self.field).searches.find_by_name(self.value).to_solr_expression
