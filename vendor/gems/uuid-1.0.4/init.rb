46e5e6100d41e1084f147bd6d0d84fec299f122a
Jamie Orchard-Hays <jamieorc@gmail.com>
2007-11-16 20:51:43 +0000

    added uuid gem frozen into vendor/gems with Dr. Nic's gemsonrails plugin. uuid is being used to generate the URIs for indexing our Exhibits

---
+++ vendor/gems/uuid-1.0.4/init.rb
@@ -0,0 +1,10 @@
+# 
+# require_options = ["uuid"]
+# if require_lib = require_options.find { |path|  File.directory?(File.join(File.dirname(__FILE__), 'lib', path)) }
+#   require File.join(File.dirname(__FILE__), 'lib', require_lib)
+# else
+#   puts msg = "ERROR: Please update #{File.expand_path __FILE__} with the require path for linked RubyGem uuid"
+#   exit
+# end
+# 
+require File.join(File.dirname(__FILE__), 'lib', 'uuid')
