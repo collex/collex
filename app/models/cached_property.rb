0062bc3bfc93346ac96c58b4a262c693f0f3af0e
Jamie Orchard-Hays <jamieorc@gmail.com>
2008-03-11 15:37:31 +0000

    Added license with 2008 copyright

--- app/models/cached_property.rb
+++ app/models/cached_property.rb
@@ -1,5 +1,5 @@
 ##########################################################################
-# Copyright 2007 Applied Research in Patacriticism and the University of Virginia
+# Copyright 2008 Applied Research in Patacriticism and the University of Virginia
 # 
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
