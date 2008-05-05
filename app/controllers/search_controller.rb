44c4d6c3f310d1b4f7a9793e292d2f482aa1b991
Nick Laiacona <nick@performantsoftware.com>
2008-05-05 18:06:56 +0000

    Changed the number of search results per page to 4 from 5 to improve responsiveness.

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -23,7 +23,7 @@ class SearchController < ApplicationController
    end
    
    def browse
-     items_per_page = 5
+     items_per_page = 4
      @page = params[:page] ? params[:page].to_i : 1
      
      
