517aee67d23f2d50e139784315443c83d5f0fce8
Jamie Orchard-Hays <jamieorc@gmail.com>
2008-08-20 22:16:08 +0000

    new model for ExhibitedUserResource--an external, user added resource for an exhibit. This uses the same ExhibitedProperty list as ExhibitedResource.

---
+++ app/models/exhibited_user_resource.rb
@@ -0,0 +1,24 @@
+##########################################################################
+# Copyright 2008 Applied Research in Patacriticism and the University of Virginia
+# 
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+##########################################################################
+
+# Models a User-contributed (non-Solr) Resource for an +Exhibit+. It also uses +ExhibitedProperty+ to
+# store the properties of the resource.
+class ExhibitedUserResource < ExhibitedItem
+  has_many :exhibited_properties, :foreign_key => "exhibited_resource_id", :dependent => :destroy
+  alias_method :properties, :exhibited_properties
+  include PropertyMethods
+  
+end
