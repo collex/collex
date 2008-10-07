4f6142d0035e1fbfb4a41b9dee9cfa3fd535551d
Paul Rosen <paul@performantsoftware.com>
2008-10-07 13:45:52 +0000

    added "all tags" parameter to the url

--- app/helpers/tag_helper.rb
+++ app/helpers/tag_helper.rb
@@ -1,23 +1,22 @@
 module TagHelper
-  def tag_cloud(list, bucket_size)
+  def tag_cloud(list, bucket_size, all_tags)
     xm = Builder::XmlMarkup.new(:indent => 2)
     list.each do |item|
 #        html = (i > NUM_VISIBLE_TAGS) ? { :style => "display:none;" } : {}
       html = {}
       xm.span :class => "cloud#{item.last.quo(bucket_size).ceil}" do
-        xm << link_to_tag(item.first, item.last, html)
+        xm << link_to_tag(item.first, item.last, all_tags, html)
       end
     end
     return xm
   end
   
   # +value+ has any ampersands changed to +&amp;+
-  def link_to_tag(value, frequency=nil, html_options = {})
+  def link_to_tag(value, frequency, all_tags, html_options = {})
      if frequency
         html_options[:title] = pluralize(frequency, 'object')
      end
-     display = value
      amped_value = value.gsub(/&amp;/, "&").gsub(/&/, "&amp;").gsub(/ \/ \/ \*$/, "")
-     link_to display, { :action => 'results', :view => 'tag', :tag => amped_value }, html_options
+     link_to value, { :action => 'results', :view => 'tag', :tag => amped_value, :all_tags=> all_tags }, html_options
   end
 end
