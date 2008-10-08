292f170408b3582fb9199857a751ec7fde97a5d5
Paul Rosen <paul@performantsoftware.com>
2008-10-08 21:38:14 +0000

    put in migration to change old collecting and tagging system to the new

---
+++ db/migrate/080_change_old_tags_to_new.rb
@@ -0,0 +1,36 @@
+class ChangeOldTagsToNew < ActiveRecord::Migration
+  def self.up
+    interpretations = Interpretation.find(:all)
+    interpretations.each { |interpretation|
+      cr = CachedResource.find_by_uri(interpretation.object_uri)
+      if cr
+        CollectedItem.create(:user_id => interpretation.user_id, :cached_resource_id => cr.id, :annotation => interpretation.annotation )
+      else
+        RAILS_DEFAULT_LOGGER.info("Interpretation (#{interpretation.id}) to CollectedItem: Can't find #{interpretation.object_uri} in CachedResource table.")
+      end
+    }
+
+    taggings = Tagging.find(:all)
+    taggings.each { |tagging|
+      interpretation = Interpretation.find_by_id(tagging.interpretation_id)
+      if interpretation
+        cr = CachedResource.find_by_uri(interpretation.object_uri)
+        if cr
+          ci = CollectedItem.find(:first, :conditions => [ "user_id = ? AND cached_resource_id = ?", interpretation.user_id, cr.id ])
+          if ci
+            Tagassign.create(:tag_id => tagging.tag_id, :collected_item_id => ci.id)
+          else
+            RAILS_DEFAULT_LOGGER.info("Tagging (#{tagging.id}) to Tagassign: Can't find user=#{interpretation.user_id}, cached resource=#{cr.id} in CollectedItem table.")
+          end
+        else
+            RAILS_DEFAULT_LOGGER.info("Tagging (#{tagging.id}) to Tagassign: Can't find #{interpretation.object_uri} in CachedResource table.")
+        end
+      else
+        RAILS_DEFAULT_LOGGER.info("Tagging (#{tagging.id}) to Tagassign: Can't find #{tagging.interpretation_id} in Interpretation table.")
+      end
+    }
+  end
+
+  def self.down
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 79) do
+ActiveRecord::Schema.define(:version => 80) do
 
   create_table "cached_properties", :force => true do |t|
     t.string  "name"
