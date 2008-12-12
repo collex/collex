286fbae97ca052af7b3076a875545cd8fe378b21
Paul Rosen <paul@performantsoftware.com>
2008-12-12 14:59:57 +0000

    another try to straighten out the migration

--- db/migrate/094_move_exhibit_section_data_to_element.rb
+++ db/migrate/094_move_exhibit_section_data_to_element.rb
@@ -1,7 +1,7 @@
-class ExhibitSection < ActiveRecord::Base
-end
-
 class MoveExhibitSectionDataToElement < ActiveRecord::Migration
+  class ExhibitSection < ActiveRecord::Base
+  end
+
   def self.up
     # Add a default border value .
     # Find what page the elements are on by looking at their section .
@@ -10,14 +10,16 @@ class MoveExhibitSectionDataToElement < ActiveRecord::Migration
     elements = ExhibitElement.find(:all)
     for element in elements
       element.border_type_enum = 0
-      section = ExhibitSection.find(element.exhibit_section_id)
-      element.exhibit_page_id = section.exhibit_page_id
-
-      pos = page_position[section.exhibit_page_id]
-      pos = 0 unless pos
-      element.position = pos + 1
-      page_position[section.exhibit_page_id] = element.position
-      element.save
+      if element.exhibit_section_id >= 1
+        section = ExhibitSection.find_by_id(element.exhibit_section_id)
+        element.exhibit_page_id = section.exhibit_page_id
+  
+        pos = page_position[section.exhibit_page_id]
+        pos = 0 unless pos
+        element.position = pos + 1
+        page_position[section.exhibit_page_id] = element.position
+        element.save
+      end
     end
   end
 
