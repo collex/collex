a02c13e0befccafe789ded2bb955bafae7df684a
Paul Rosen <paul@performantsoftware.com>
2008-12-15 15:30:20 +0000

    Make the onchange work when changing the selection in a select control.

--- public/javascripts/input_dialog.js
+++ public/javascripts/input_dialog.js
@@ -131,7 +131,9 @@ InputDialog.prototype = {
 			wrapper.addClassName(className);
 		var el_label = new Element('label', { 'for': id} ).update(label);
 		wrapper.appendChild(el_label.wrap('td'));
-		var el = new Element('select', { id: id, name: id, align: 'top', onchange: change });
+		var el = new Element('select', { id: id, name: id, align: 'top' });
+		if (change != null && change.length > 0)
+			el.observe('change', change);
 		options.each(function(option) {
 			el.appendChild(new Element('option', { value: option}).update(option));
 		});
