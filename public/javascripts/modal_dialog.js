20e86e761b95576a9aa80a000b24aa579b5eb950
Paul Rosen <paul@performantsoftware.com>
2008-12-17 04:38:57 +0000

    * The caption "Text Editing Tools" appears on the rich editor. RTE
    * Remove font color, image embed from text editor. RTE

--- public/javascripts/modal_dialog.js
+++ public/javascripts/modal_dialog.js
@@ -78,7 +78,79 @@ ModalDialog.prototype = {
 			//create the RTE:
 			this.editor = new YAHOO.widget.SimpleEditor(textArea.id, {
 				  width: '702px',
-					height: '200px'
+					height: '200px',
+					toolbar: {
+						//titlebar: 'My tool',
+						draggable: false,
+						buttons: [{
+							group: 'fontstyle',
+							label: 'Font Name and Size',
+							buttons: [{
+								type: 'select',
+								label: 'Arial',
+								value: 'fontname',
+								disabled: true,
+								menu: [{ text: 'Arial', checked: true },
+									{ text: 'Arial Black' },
+									{ text: 'Comic Sans MS' },
+									{ text: 'Courier New' },
+									{ text: 'Lucida Console' },
+									{ text: 'Tahoma' },
+									{ text: 'Times New Roman' },
+									{ text: 'Trebuchet MS' },
+									{ text: 'Verdana' } ]},
+								{
+								type: 'spin',
+								label: '13',
+								value: 'fontsize',
+								range: [9, 75],
+								disabled: true
+							}]
+						}, {
+							type: 'separator'
+						}, {
+							group: 'textstyle',
+							label: 'Font Style',
+							buttons: [{
+								type: 'push',
+								label: 'Bold CTRL + SHIFT + B',
+								value: 'bold'
+							}, {
+								type: 'push',
+								label: 'Italic CTRL + SHIFT + I',
+								value: 'italic'
+							}, {
+								type: 'push',
+								label: 'Underline CTRL + SHIFT + U',
+								value: 'underline'
+							}]
+						}, {
+							type: 'separator'
+						}, {
+							group: 'indentlist',
+							label: 'Lists',
+							buttons: [{
+								type: 'push',
+								label: 'Create an Unordered List',
+								value: 'insertunorderedlist'
+							}, {
+								type: 'push',
+								label: 'Create an Ordered List',
+								value: 'insertorderedlist'
+							}]
+						}, {
+							type: 'separator'
+						}, {
+							group: 'insertitem',
+							label: 'Insert Item',
+							buttons: [{
+								type: 'push',
+								label: 'HTML Link CTRL + SHIFT + L',
+								value: 'createlink',
+								disabled: true
+							}]
+						}]
+					}
 			});
 
 			//attach the Editor's reusable property-editor
