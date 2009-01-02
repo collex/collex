56a1a5271aed52e6f54feecd2e71fecc1b9fb7e1
Paul Rosen <paul@performantsoftware.com>
2009-01-02 16:55:48 +0000

    Added copyright notice on files that had none.

--- public/javascripts/initialize_inplacericheditor.js
+++ public/javascripts/initialize_inplacericheditor.js
@@ -1,11 +1,23 @@
-/**
- * @author paulrosen
- * This contains the glue code between the input_dialog object and the HTML elements
- * that the user is able to click on to edit. To use, pass in the element_id and the callback
- * URL that will report the user's changes. There is an entry point for each type of popup
- * dialog that should be displayed.
- * 
- */
+//------------------------------------------------------------------------
+//    Copyright 2009 Applied Research in Patacriticism and the University of Virginia
+//    
+//    Licensed under the Apache License, Version 2.0 (the "License");
+//    you may not use this file except in compliance with the License.
+//    You may obtain a copy of the License at
+//  
+//        http://www.apache.org/licenses/LICENSE-2.0
+//  
+//    Unless required by applicable law or agreed to in writing, software
+//    distributed under the License is distributed on an "AS IS" BASIS,
+//    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//    See the License for the specific language governing permissions and
+//    limitations under the License.
+//----------------------------------------------------------------------------
+
+//This contains the glue code between the input_dialog object and the HTML elements
+//that the user is able to click on to edit. To use, pass in the element_id and the callback
+//URL that will report the user's changes. There is an entry point for each type of popup
+//dialog that should be displayed.
 
 var _inplaceObjects = [];
 var _inplaceObjectsAlreadyLoaded = false;
