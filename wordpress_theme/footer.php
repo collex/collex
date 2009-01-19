19253dcbb070e7691e83940415984debb565a257
Paul Rosen <paul@performantsoftware.com>
2009-01-19 21:10:15 +0000

    Added google analytics to wordpress

--- wordpress_theme/footer.php
+++ wordpress_theme/footer.php
@@ -25,5 +25,16 @@
 <?php /* "Just what do you think you're doing Dave?" */ ?>
 
 		<?php wp_footer(); ?>
+	<!-- for Google Analytics -->
+	<script type="text/javascript">
+		var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");
+		document.write(unescape("%3Cscript src='" + gaJsHost + "google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E"));
+	</script>
+	<script type="text/javascript">
+		try {
+		var pageTracker = _gat._getTracker("UA-5450014-1");
+		pageTracker._trackPageview();
+		} catch(err) {}
+	</script>
 </body>
 </html>
