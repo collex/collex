e1008884a776c408379c51fd404d5e1fe396c4fc
Paul Rosen <paul@performantsoftware.com>
2009-04-07 15:36:14 +0000

    bug: exhibit url going back to id on page 2 of exhibit

--- app/helpers/exhibit_elements_helper.rb
+++ app/helpers/exhibit_elements_helper.rb
@@ -1,6 +1,9 @@
 module ExhibitElementsHelper
+  def get_exhibit_id(exhibit)
+    return exhibit.visible_url && exhibit.visible_url.length > 0 ? exhibit.visible_url : exhibit.id
+  end
   def get_exhibit_url(exhibit)
-    return "/exhibits/#{exhibit.visible_url && exhibit.visible_url.length > 0 ? exhibit.visible_url : exhibit.id}"
+    return "/exhibits/#{get_exhibit_id(exhibit)}"
   end
   def get_exhibit_link(exhibit)
     return "<a class='nav_link' href='#{get_exhibit_url(exhibit)}'>#{h exhibit.title}</a>"
