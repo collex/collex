2db455d47a29ee346632ce60bd282dfbb8abfa3a
Nick Laiacona <nick@performantsoftware.com>
2009-06-11 19:47:17 +0000

    fixing article nav

--- wordpress_theme/single.php
+++ wordpress_theme/single.php
@@ -12,9 +12,11 @@ get_header();
 	<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 
 		<div class="navigation">
-			<div class="alignleft"><?php previous_post_link('&laquo; Previous') ?></div>
-			<div class="alignright"><?php next_post_link('&raquo; Next') ?></div>
-			<div class="aligncenter"><a href="<?php bloginfo('url'); ?>">News Home</a></div>
+			<div class="alignleft">&laquo;</div>
+			<div class="alignleft"><?php previous_post_link('%link', 'Previous', TRUE ) ?></div>
+			<div class="alignright">&raquo;</div>
+			<div class="alignright"><?php next_post_link('%link', 'Next', TRUE ) ?></div>
+			<div class="aligncenter"><a href="<?php bloginfo('url'); ?>">Return to News</a></div>
 		</div>
 
 		<div <?php post_class() ?> id="post-<?php the_ID(); ?>">
