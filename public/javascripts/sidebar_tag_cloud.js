8f1baf8b74bda6318e36f434fe3ebd1b5be0e8d6
Paul Rosen <paul@performantsoftware.com>
2009-06-24 20:26:25 +0000

    Remove unused part of tag cloud javascript; expand tags if they are partially hidden when the user filters.

--- public/javascripts/sidebar_tag_cloud.js
+++ public/javascripts/sidebar_tag_cloud.js
@@ -18,20 +18,13 @@ SidebarTagCloud = Class.create( {
 	
 	initialize: function( ) {				
 		this.instructions = { 'tag': 'tag', 'annotation': 'annotation' };
-		this.initialCloudSize = 50000;
 		this.initSidebarFilterHandler();	
-	},
-	
-	onSidebarLoadComplete: function() {
-		this.spinner.stop();
-		this.initSidebarFilterHandler();					
 	},
 	
 	initSidebarFilterHandler: function() {
 		// initialize keystroke event for sidebar filter field
-		this.showAllCloudTags = false;
 		this.sidebarTouched = false;
-		this.updateTagCloud();
+		//this.updateTagCloud();
 		Event.observe('sidebar_search', 'keyup', this.onSidebarFilter.bindAsEventListener(this) );					
 	},
 
@@ -69,60 +62,10 @@ SidebarTagCloud = Class.create( {
 			}
 		});
 
-		// hide excess tags if we are not showing all tags
-		if( this.showAllCloudTags == false && visibleTags.length > this.initialCloudSize ) {
-			var tagIndex = this.initialCloudSize; 
-			while( tagIndex < visibleTags.length ) {
-				visibleTags[tagIndex++].hide();
-			}		
-		}	
-	},
-
-	// toggle whether we show all tags or a subset
-	toggleShowMoreCloudTags: function() {
-		this.showAllCloudTags = !this.showAllCloudTags;
-		var toggleButton = $('toggle-show-all-tags');
-		if( this.showAllCloudTags ) {
-			toggleButton.innerHTML = "hide more";
-		} else {
-			toggleButton.innerHTML = "show more";		
-		}	
-		this.updateTagCloud();
-	},
-
-	// update the sidebar with new content from the target URL
-	updateSidebar: function( targetURL ) {		
-
-		// hide the content
-		$$('.content').invoke('hide');
-		
-		$('sidebar-spinner').show();
-		
-		// display a spinner
-		this.spinner = new Spinner( 'sidebar-spinner', { height: 48, width: 48, speed: 0.1, image: '/images/spinner.png'} );
-		
-		new Ajax.Updater('sidebar', targetURL, {
-			asynchronous:true, 
-			evalScripts:true,
-			onComplete: this.onSidebarLoadComplete.bindAsEventListener(this)
-		});
-	},	
-		
-	defaultInstructions: function(t) {
-	  var key = t.name.split('-',1)[0];
-	  return this.instructions[key];
-	},
-
-	clearInstructions: function(t) {
-	  if (t.value == this.defaultInstructions(t)) {t.value = '';}
-	},
-
-	check_submit: function() {		
-	    if (document.forms['dataform'].tag.value == '' || document.forms['dataform'].tag.value == this.instructions['tag'] ) { 
-	      alert('Sorry! you must assign at least one keyword in order to collect an object.'); 
-	      return false; 
-	    }
-	    return true;
-	}				
+		// If some tags are hidden, then show them when the filter is used.
+		var moreTags = $('more_tags');
+		if (moreTags)
+			moreTags.show();
+	}
 });
 
