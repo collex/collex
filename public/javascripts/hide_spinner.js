c89c08a1fa51675e308d55df72c346d4f97f4994
Paul Rosen <paul@performantsoftware.com>
2009-07-31 20:51:03 +0000

    Compensate for IE not reporting the image size correctly.

--- public/javascripts/hide_spinner.js
+++ public/javascripts/hide_spinner.js
@@ -33,44 +33,50 @@ function finishedLoadingImage(progress_el, img_el, max_width, max_height)
 	// Since the optimum placement (max_width X max_height) may not be square, we'll just figure it out.
 	var natural_width = $(img_el).width;
 	var natural_height = $(img_el).height;
+	if (natural_width === 0 && natural_height === 0) {	// IE doesn't properly report the width and height, so we'll fudge it.
+		// TODO-PER: This will distort the images in IE, but at least they won't be too big.
+		$(img_el).width = max_width;
+		$(img_el).height =  max_height;
+	} else {
 
-	// We'll set zero or one of these below, depending on the picture's natural dimensions.
-	var width = null;
-	var height = null;
-	if ((natural_width <= max_width) && (natural_height <= max_height))	// If the image is completely smaller than the opening, then don't limit either dimension.
-		width = null;	// don't need to do anything here.'
-	else if ((natural_width <= max_width) && (natural_height > max_height))	// Only the height is too big, so limit the height
-		height = max_height;
-	else if ((natural_width > max_width) && (natural_height <= max_height))	// Only the width is too big, so limit the width
-		width = max_width;
-	else	// both the height and width are too big, so figure out which is more too big than the other.
-	{
-		var width_percent_over = natural_width / max_width;
-		var height_percent_over = natural_height / max_height;
-		if (width_percent_over > height_percent_over)
-			width = max_width;
-		else
+		// We'll set zero or one of these below, depending on the picture's natural dimensions.
+		var width = null;
+		var height = null;
+		if ((natural_width <= max_width) && (natural_height <= max_height))	// If the image is completely smaller than the opening, then don't limit either dimension.
+			width = null;	// don't need to do anything here.'
+		else if ((natural_width <= max_width) && (natural_height > max_height))	// Only the height is too big, so limit the height
 			height = max_height;
-	}
+		else if ((natural_width > max_width) && (natural_height <= max_height))	// Only the width is too big, so limit the width
+			width = max_width;
+		else	// both the height and width are too big, so figure out which is more too big than the other.
+		{
+			var width_percent_over = natural_width / max_width;
+			var height_percent_over = natural_height / max_height;
+			if (width_percent_over > height_percent_over)
+				width = max_width;
+			else
+				height = max_height;
+		}
 
-	if (width) {
-		$(img_el).width = width;
-		// Now center the image vertically
-		var new_height = $(img_el).height;
-		var padding = (max_height - new_height) / 2;
-		if (padding > 0)
-			$(img_el).setStyle({ paddingTop: padding + "px" });
-	}
+		if (width) {
+			$(img_el).width = width;
+			// Now center the image vertically
+			var new_height = $(img_el).height;
+			var padding = (max_height - new_height) / 2;
+			if (padding > 0)
+				$(img_el).setStyle({ paddingTop: padding + "px" });
+		}
 
-	if (height) {
-		$(img_el).height = height;
-		// Now center the image horizontally
-		var new_width = $(img_el).width;
-		var padding2 = (max_width - new_width) / 2;
-		if (padding2 > 0)
-			$(img_el).setStyle({ paddingLeft: padding2 + "px" });
+		if (height) {
+			$(img_el).height = height;
+			// Now center the image horizontally
+			var new_width = $(img_el).width;
+			var padding2 = (max_width - new_width) / 2;
+			if (padding2 > 0)
+				$(img_el).setStyle({ paddingLeft: padding2 + "px" });
+		}
 	}
-
+	
 	$(progress_el).addClassName('hidden');
 	$(img_el).removeClassName('hidden');
 }
