ad8a1573d7e5f8bc1cc0fd1b2ca900029b329f22
Paul Rosen <paul@performantsoftware.com>
2009-10-21 18:01:32 +0000

    Try to make migration work from a clean database.

--- db/migrate/038_create_facet_categories.rb
+++ db/migrate/038_create_facet_categories.rb
@@ -22,28 +22,28 @@ class CreateFacetCategories < ActiveRecord::Migration
 #      t.column :type, :string
 #    end
 
-    # Create initial set of archive facet categories    
-    archive = FacetTree.create(:value => 'archive')
-
-    # Categories under the archive facet tree
-    libraries = FacetCategory.new(:value => 'Libraries')
-    journals = FacetCategory.new(:value => 'Journals')
-    presses = FacetCategory.new(:value => 'Presses')
-    projects = FacetCategory.new(:value => 'Projects')
-    
-    archive << libraries
-    archive << journals
-    archive << presses
-    archive << projects
-    
-    # Libraries
-    # Journals
-    # Presses
-    
-    # Projects
-    projects << FacetValue.new(:value => 'rossetti')
-    
-    archive.save
+#    # Create initial set of archive facet categories
+#    archive = FacetTree.create(:value => 'archive')
+#
+#    # Categories under the archive facet tree
+#    libraries = FacetCategory.new(:value => 'Libraries')
+#    journals = FacetCategory.new(:value => 'Journals')
+#    presses = FacetCategory.new(:value => 'Presses')
+#    projects = FacetCategory.new(:value => 'Projects')
+#
+#    archive << libraries
+#    archive << journals
+#    archive << presses
+#    archive << projects
+#
+#    # Libraries
+#    # Journals
+#    # Presses
+#
+#    # Projects
+#    projects << FacetValue.new(:value => 'rossetti')
+#
+#    archive.save
   end
 
   def self.down
