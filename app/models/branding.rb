61c9d006923491b555c22c948289d2b5477ee57d
Paul Rosen <paul@performantsoftware.com>
2009-10-28 17:02:56 +0000

    Put up "maintenence" notice for all hits on the site -- this is for the old nines.org site while the new site goes live.

--- app/models/branding.rb
+++ app/models/branding.rb
@@ -18,7 +18,7 @@ class Branding
 	# This contains the functional differences between Collex and the specific implementation of it.
 	# In this case, this is the NINES specific stuff
   def self.version
-    return "1.5.12"
+    return "1.5.13"
   end
 
 	def self.yui_path(is_debug)
