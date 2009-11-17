39a0563be7c2a7902446fcc37858c28b7dc6fa2c
Paul Rosen <paul@performantsoftware.com>
2009-11-17 22:54:42 +0000

    Bug: couldn't start a discussion from the my9s page.

--- app/models/get_include_file_list.rb
+++ app/models/get_include_file_list.rb
@@ -51,7 +51,7 @@ class GetIncludeFileList
 			when :tag
 				local = [ 'sidebar_tag_cloud', 'tag_zoom', 'result_row_popup', 'cc_license' ]
 			when :my9s
-				local = [ 'initialize_inplacericheditor', 'sidebar_tag_cloud', 'edit_exhibit', 'result_row_popup', 'thumbnail_resize', 'saved_search', 
+				local = [ 'initialize_inplacericheditor', 'sidebar_tag_cloud', 'edit_exhibit', 'result_row_popup', 'thumbnail_resize', 'saved_search', 'cc_license',
 					'border_dialog', 'edit_exhibit_object_list_dlg', 'set_author_alias_dlg', 'create_new_exhibit_dlg', 'edit_user_profile_dlg', 'footnotes', 'renumber_footnotes' ]
 			when :discuss
 				local = [ 'discussions', 'result_row_popup', 'cc_license' ]
