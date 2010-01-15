cd8fece4a4f319b354c82c9adc5b92557e82d932
Paul Rosen <paul@performantsoftware.com>
2010-01-15 18:32:59 +0000

    Wording change on new exhibit starting text.

--- app/models/exhibit.rb
+++ app/models/exhibit.rb
@@ -109,7 +109,11 @@ class Exhibit < ActiveRecord::Base
       type2 = 'pic_text'
     end
     el = new_page.insert_element(1)
-    el.element_text = "Welcome to your new exhibit. Click here to enter text, or select another layout from the section editing toolbar above."
+		if page_num == 1
+	    el.element_text = "Welcome to your new exhibit. Click here to enter text, or select another layout from the section editing toolbar above."
+		else
+	    el.element_text = "Enter your text here."
+		end
     #el.element_text = "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
     
     el.save
