4b63f7dfa1e298d394bf99a57c3d1a8fa7c25c3b
Lou Foster <lou@performantsoftware.com>
2011-05-11 12:59:45 +0000

    added * to the troublesome character strip

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -155,7 +155,7 @@ class SearchController < ApplicationController
        # strip punctuation and other troublesome chars
        # NOTE: for some reason, the escaped brackets only work in ruby regex if
        # they are in the middle of the expression. 
-       word.gsub!(/[.\^\|,\[\]!}{?+-]/, "")
+       word.gsub!(/[.*\^\|,\[\]!}{?+-]/, "")
        
        if word.upcase == "NOT"
          # set a flag so the next word found will be inverted
