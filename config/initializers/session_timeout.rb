a3a29a0837c385a828f318ec7d4159702400ed3d
Paul Rosen <paul@nines.org>
2012-01-10 12:00:03 -0500

    Make session timeout work

--- config/initializers/session_timeout.rb
+++ config/initializers/session_timeout.rb
@@ -1,1 +1,14 @@
-Rails.application.config.idle_session_timeout = 4*60 #minutes
+#Rails.application.config.idle_session_timeout = 4*60 #minutes
+Collex::Application.config.idle_session_timeout = 4*60 #minutes
+
+# We want the session to time out always, not only when logged in.
+# Also, the criterion for whether the session is logged in is not recognized by Collex.
+module Ixtlan
+	module Sessions
+		module Timeout
+			def logged_in?
+				return true
+			end
+		end
+	end
+end
