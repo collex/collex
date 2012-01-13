b3fc312f3a16583389917431d9a6ffa1fcb6f55e
Paul Rosen <paul@nines.org>
2012-01-13 10:16:15 -0500

    Invalidate all cookies for this upgrade.

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Collex::Application.config.secret_token = 'c974029bae62a0c12853102ff91a9f8317f5ba5979f20cdefd01cece75bdbce70f942265311aaa5c9c086747d0794748e1652c3df429ee7a553fd64e951a732d'
+Collex::Application.config.secret_token = 'c974029bae62a0c12753102ff91a9f8317f5ba5979f20cdefd01cece75bdbce70f942265311aaa5c9c086747d0794748e1652c3df429ee7a553fd64e951a732d'
