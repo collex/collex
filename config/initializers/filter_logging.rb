993da31afbf59677b637ce54e36ef16b30ce960b
Paul Rosen <paul@nines.org>
2012-02-03 17:06:37 -0500

    Filter out the Rendered messages from the log

--- config/initializers/filter_logging.rb
+++ config/initializers/filter_logging.rb
@@ -1,5 +1,5 @@
 # This is for rails 3.2.1. Doesn't work in 3.1.3
-* skip "Rendered partial..." lines
+#* skip "Rendered partial..." lines
 module ActiveSupport
   class BufferedLogger
     def add(severity, message = nil, progname = nil, &block)
