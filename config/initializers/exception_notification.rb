17db13d50c16606a50310f56bdc5f93e207c9632
Lou Foster <lou@performantsoftware.com>
2013-08-09 15:06:40 -0400

    update exception notification init

--- config/initializers/exception_notification.rb
+++ config/initializers/exception_notification.rb
@@ -1,8 +1,10 @@
 if Rails.env.to_s != 'development'
 	except = Setup.exception_notifier()
-	Collex::Application.config.middleware.use ExceptionNotifier,
+	Collex::Application.config.middleware.use ExceptionNotification::Rack,
+	:email => {
 		:email_prefix => except[:prefix],
 		:sender_address => except[:sender],
 		:exception_recipients => except[:recipients],
 		:ignore_crawlers      => %w{Googlebot bingbot AhrefsBot JikeSpider}
+  }
 end
