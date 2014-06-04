200632832a64bf883905fc92957f13308db5e2d8
Paul Rosen <paul@paulrosen.net>
2014-06-04 17:22:27 -0400

    Quick pass at skinning modnets: just a few assets included so far.

--- app/models/branding.rb
+++ app/models/branding.rb
@@ -17,7 +17,7 @@
 class Branding
 	# Here are some version numbers that we'd like to keep together.
   def self.version	# Don't change the format of this call because collex.rake depends on it!
-    return "1.7.0"
+    return "1.7.1"
   end
 
 	def self.yui_path()
