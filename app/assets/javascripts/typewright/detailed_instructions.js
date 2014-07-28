eb34cb2ca00a38725043c21707f1df4bd7bb7cf0
Paul Rosen <paul@paulrosen.net>
2014-07-28 10:14:52 -0400

    Removed YUI reference in typewright.

--- app/assets/javascripts/typewright/detailed_instructions.js
+++ app/assets/javascripts/typewright/detailed_instructions.js
@@ -3,19 +3,18 @@
 // Requires the general_dialog file's showPartialInLightBox
 // requires the calling element has data-url defined, which is the partial to display.
 
-/*global YUI */
 /*global showPartialInLightBox */
 
-YUI().use('node', function(Y) {
+jQuery(document).ready(function() {
 	"use strict";
 	function display_lightbox(node) {
-		var url = node._node.getAttribute('data-url');
-		var title = node._node.getAttribute('data-title');
+		var url = node.attr('data-url');
+		var title = node.attr('data-title');
 		showPartialInLightBox(url, title, '/assets/ajax_loader.gif');
 
 	}
 
-    Y.on("click", function(e) {
-        display_lightbox(e.target);
-    }, ".show-in-lightbox");
+	jQuery(".show-in-lightbox").on("click", function() {
+        display_lightbox(jQuery(this));
+    });
 });
